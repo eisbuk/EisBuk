a0508c83e0f7590ae3a09a4e5de0f7db1ab3763f
fadwa <fadwa.mahmoud@gmail.com>
2021-07-20 21:36:50 +0200

    Basic i18next setup

---
+++ eisbuk-admin/src/i18next/i18n.ts
@@ -0,0 +1,21 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+
+// import ns1 from 'locales/en/ns1.json';
+// import ns2 from 'locales/en/ns2.json';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    debug: true,
+    fallbackLng: "en",
+  });
+
+export default i18n;
