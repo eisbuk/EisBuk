91693e86b1744634d5c02909c174cba85efd280a
ikusteu <ivan.kust57@gmail.com>
2021-07-22 10:40:16 +0200

    Add removed file to git

--- eisbuk-admin/fix-jsdom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This PR lays unmerged, so we apply it with this script:
-# https://github.com/jsdom/jsdom/pull/3073
-
-find node_modules/ -name xhr-utils.js -exec patch -N -r- {} ./jsdom.patch \;
