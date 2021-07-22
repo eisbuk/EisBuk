2fbaf2f2162a7b105bf2684c9b9460387f0d8152
ikusteu <ivan.kust57@gmail.com>
2021-07-22 10:24:53 +0200

    Remove envInfoEnv.ts hack

--- eisbuk-admin/src/lib/constants.ts
+++ eisbuk-admin/src/lib/constants.ts
@@ -3,4 +3,4 @@ export const __storybookDate__ = process.env.STORYBOOK_DATE;
 export const __isStorybook__ = Boolean(process.env.STORYBOOK_IS_STORYBOOK);
 
 // dev organization constants
-export const __organization__ = process.env.REACT_APP_ORGANIZATION;
+export const __eisbukSite__ = process.env.REACT_APP_EISBUK_SITE;
