43d1ef4c50ac3275f1b68ce5dd11942648ff78c7
fadwa <fadwa.mahmoud@gmail.com>
2021-08-29 17:26:26 +0200

    add comments

--- eisbuk-admin/src/components/atoms/AttendanceCard/AttendanceCard.tsx
+++ eisbuk-admin/src/components/atoms/AttendanceCard/AttendanceCard.tsx
@@ -20,13 +20,17 @@ const AttendanceCard: React.FC<Props> = ({
   absentees,
 }) => {
   const dispatch = useDispatch();
+  // convert timestamp to luxon for easier processing
 
   const luxonStart = fb2Luxon(date);
+  // convert durations to number values
 
   const durationNumbers = durations.map((duration) => Number(duration));
   const longestDuration = Math.max(...durationNumbers);
+  // get end time of longest duration (we're still using durations here, so it's pretty straightforward)
 
   const luxonEnd = luxonStart.plus({ minutes: longestDuration });
+  // get time for rendering
 
   const startTime = luxonStart.toISOTime().substring(0, 5);
   const endTime = luxonEnd.toISOTime().substring(0, 5);
