ea7cf6ce7d436ab2f4b133670b24977e2f71255f
fadwa <fadwa.mahmoud@gmail.com>
2021-09-11 15:38:15 +0200

    sorting out props and functionality in userAttendance

--- eisbuk-admin/src/components/atoms/UserAttendance/UserAttendance.tsx
+++ eisbuk-admin/src/components/atoms/UserAttendance/UserAttendance.tsx
@@ -4,91 +4,76 @@ import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import EisbukAvatar from "@/components/users/EisbukAvatar";
-import { Customer, BookingsMeta, Slot } from "eisbuk-shared";
+import { Customer } from "eisbuk-shared";
 import Button from "@material-ui/core/Button";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import { ETheme } from "@/themes";
-import {
-  markAbsence,
-  markAttendance,
-} from "@/store/actions/attendanceOperations";
+
 import { useDispatch } from "react-redux";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
 import _ from "lodash";
-import { SlotInterface } from "@/types/temp";
-
-type UserBooking = BookingsMeta & Pick<Customer, "certificateExpiration">;
+import { SlotInterface, CustomerAttendance } from "@/types/temp";
 
 interface Props {
-  attended: boolean;
-  userBooking: UserBooking;
-  slotId: Slot<"id">["id"];
+  customer: Customer;
+  attendance: CustomerAttendance;
   intervals: SlotInterface["intervals"];
+  markAttendance: () => void;
+  markAbsence: () => void;
 }
 
 const UserAttendance: React.FC<Props> = ({
-  attended,
-  userBooking,
-  slotId,
+  customer,
+  attendance,
   intervals,
+  markAttendance,
+  markAbsence,
 }) => {
-  const [localAttended, setLocalAttended] = useState(attended);
-
   const classes = useStyles();
   const dispatch = useDispatch();
+
+  const { attended, booked } = attendance;
+  const [localAttended, setLocalAttended] = useState(attended);
+  const [selectedInterval, setSelectedInterval] = useState(booked);
   const listItemClass = attended ? "" : classes.absent;
 
   const handleClick = () => {
-    const newAttended = !attended;
+    const newAttended = attended && null;
     setLocalAttended(newAttended);
-    dispatch(
-      newAttended
-        ? markAttendance({
-            slotId,
-            customerId: userBooking.customer_id,
-            attendedInterval: "",
-          })
-        : markAbsence({ slotId, customerId: userBooking.customer_id })
-    );
+    dispatch(newAttended ? markAttendance() : markAbsence());
   };
   const handleSelect = (interval: string) => {
-    dispatch(
-      selectInterval({
-        slotId,
-        userId: userBooking.customer_id,
-        interval,
-      })
-    );
+    setSelectedInterval(interval);
   };
 
   const absenteeButtons = (
     <FormControl className={classes.formControl}>
       <InputLabel>Intervals</InputLabel>
       <Select
-        data-testid={`${userBooking.name}-${userBooking.surname}-select`}
+        data-testid="select"
         value="13:00 - 14:00"
         onChange={() => handleSelect("13:15 - 14:15")}
         // materialUI <select> element is an abstraction for a bunch of other elements
         // inputProps sets the testid of <input> element inside the <select>
         // so that rtl can fireEvents on it
         inputProps={{
-          "data-testid": `${userBooking.name}-${userBooking.surname}-input`,
+          "data-testid": "input",
           disabled: !localAttended,
         }}
       >
         {_.entries(intervals).map((interval) => (
           <option
-            key={`${userBooking.name}${userBooking.surname}${interval}`}
-            value={interval[0]}
+            key={`${customer.name}${customer.surname}${interval}`}
+            value={booked!}
           >
             {interval}
           </option>
         ))}
       </Select>
       <Button
-        data-testid={`${userBooking.name}-${userBooking.surname}`}
+        data-testid="attendance-button"
         variant="contained"
         size="small"
         color={attended ? "primary" : "secondary"}
@@ -103,9 +88,9 @@ const UserAttendance: React.FC<Props> = ({
   return (
     <ListItem className={listItemClass}>
       <ListItemAvatar>
-        <EisbukAvatar {...userBooking} />
+        <EisbukAvatar {...customer} />
       </ListItemAvatar>
-      <ListItemText primary={userBooking.name} />
+      <ListItemText primary={customer.name} />
       <ListItemSecondaryAction>{absenteeButtons}</ListItemSecondaryAction>;
     </ListItem>
   );
