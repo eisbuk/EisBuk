92398bd65b9b459bf2cae1bd5fe9d711188dcfd4
ikusteu <ivan.kust57@gmail.com>
2021-09-23 17:16:10 +0200

    Update BookingCalendar to new data model

--- eisbuk-admin/src/pages/customer_area/BookingsCalendar/BookingsCalendar.tsx
+++ eisbuk-admin/src/pages/customer_area/BookingsCalendar/BookingsCalendar.tsx
@@ -5,32 +5,54 @@ import Grid from "@material-ui/core/Grid";
 
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
+import { SlotsById } from "eisbuk-shared";
+
+import { LocalStore } from "@/types/store";
+
 import BookingCard from "@/components/atoms/BookingCard";
+import { BookingCardVariant } from "@/enums/components";
 
 interface Props {
   /**
-   * Array of bookings to display -> booked slot data + duration
+   * List of slots for given month.
+   * We're filtering those based on bookings
    */
-  // bookings?: Array<BookingCardProps & { id: string }>;
-  bookings?: Array<any & { id: string }>;
+  slots: SlotsById;
+  /**
+   * Booked slots we're using as a filter mask to render only the booked slots with proper booked intervals
+   */
+  bookedSlots: LocalStore["firestore"]["data"]["bookedSlots"];
 }
 
 /**
  * A component used by `customers` page to render the booked slots as `BookingCard`s.
  */
-const BookingsCalendar: React.FC<Props> = ({ bookings }) => {
+const BookingsCalendar: React.FC<Props> = ({ slots, bookedSlots }) => {
   const classes = useStyles();
+
+  const bookedSlotIds = Object.keys(bookedSlots || {});
 
   return (
     <DateNavigation jump="week">
       {() => (
         <>
           <Grid className={classes.bookingsListContainer} container spacing={3}>
-            {bookings?.map((booking) => (
-              <Grid key={booking.id} item xs={12} sm={6} md={4} lg={3}>
-                <BookingCard {...booking} />
-              </Grid>
-            ))}
+            {bookedSlotIds.map((slotId) => {
+              const { intervals, ...slotData } = slots[slotId];
+              const bookedIntervalString = bookedSlots
+                ? bookedSlots[slotId].interval
+                : "";
+              const interval = intervals[bookedIntervalString];
+
+              return (
+                <Grid key={slotId} item xs={12} sm={6} md={4} lg={3}>
+                  <BookingCard
+                    {...{ ...slotData, interval }}
+                    variant={BookingCardVariant.Calendar}
+                  />
+                </Grid>
+              );
+            })}
           </Grid>
         </>
       )}
--- eisbuk-admin/src/pages/customer_area/BookingsCalendar/__tests__/BookingsCalendar.test.tsx
+++ eisbuk-admin/src/pages/customer_area/BookingsCalendar/__tests__/BookingsCalendar.test.tsx
@@ -1,50 +1,30 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
-import { DateTime } from "luxon";
+import * as reactRedux from "react-redux";
 
 import BookingsCalendar from "../BookingsCalendar";
 
-import { luxon2ISODate } from "@/utils/date";
+import { changeCalendarDate } from "@/store/actions/appActions";
 
-import { bookedSlots } from "../__testData__/dummyData";
+import { bookedSlots, slots } from "../__testData__/dummyData";
 import { __dateNavNextId__ } from "@/__testData__/testIds";
+import { testDateLuxon } from "@/__testData__/date";
 
-// mock date we'll be using throughout tests
-const testDateISO = "2021-03-01";
-const testDate = DateTime.fromISO(testDateISO);
+const mockDispatch = jest.fn();
+jest.spyOn(reactRedux, "useDispatch").mockImplementation(() => mockDispatch);
+// we're only reading calendar day from `useSelector`
+jest.spyOn(reactRedux, "useSelector").mockImplementation(() => testDateLuxon);
 
-// test string for users secret key
-const testSecret = "test-secret-key";
-
-/**
- * Mock function we'll be using to test `react-router`'s `history.push`
- */
-const mockHistoryPush = jest.fn();
-jest.mock("react-router-dom", () => ({
-  useHistory: () => ({ push: mockHistoryPush }),
-  useParams: () => ({
-    date: "2021-03-01",
-    secretKey: "test-secret-key",
-  }),
-  useLocation: () => ({ pathname: "/customers/test-secret-key/2021-03-01" }),
-}));
-
-/**
- * @TODO The test suite is skipped for now. The test would be passing if it weren't for MUI's `makeStyles`,
- * which is somewhat difficult to mock.
- * We should really consider using TailwindCSS for styles to eliminate this kind of problems.
- */
-xdescribe("BookingsCalendar", () => {
+describe("BookingsCalendar", () => {
   describe("Test pagination", () => {
     test("should paginate by week", () => {
-      render(<BookingsCalendar bookings={bookedSlots} />);
+      render(<BookingsCalendar {...{ bookedSlots, slots }} />);
       screen.getByTestId(__dateNavNextId__).click();
       // we're expecting next date to be a month jump from our first date
-      const expectedDate = testDate.plus({ week: 1 });
-      const expectedDateISO = luxon2ISODate(expectedDate);
-      // apply expected date to create an expected route
-      const expectedNewRoute = `/customers/${testSecret}/${expectedDateISO}`;
-      expect(mockHistoryPush).toHaveBeenCalledWith(expectedNewRoute);
+      const expectedDate = testDateLuxon.plus({ week: 1 });
+      expect(mockDispatch).toHaveBeenCalledWith(
+        changeCalendarDate(expectedDate)
+      );
     });
   });
 });
