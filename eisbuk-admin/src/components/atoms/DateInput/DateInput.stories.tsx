985e6b60d53f717ddd8aba1eb39ff7fcf826f2c4
fadwa <fadwa.mahmoud@gmail.com>
2021-10-15 23:53:02 +0200

    dateInput error and icon stories

--- eisbuk-admin/src/components/atoms/DateInput/DateInput.stories.tsx
+++ eisbuk-admin/src/components/atoms/DateInput/DateInput.stories.tsx
@@ -9,13 +9,30 @@ import DateInput from "./DateInput";
 export default {
   title: "Date Input",
   component: DateInput,
-  decorators: [
-    (Story) => (
-      <Formik initialValues={{}} onSubmit={() => {}}>
-        <Story />
-      </Formik>
-    ),
-  ],
+  // decorators: [
+  //   (Story) => (
+  //     <Formik initialValues={{}} onSubmit={() => {}}>
+  //       <Story />
+  //     </Formik>
+  //   ),
+  // ],
 } as ComponentMeta<typeof DateInput>;
 
-export const Default = (): JSX.Element => <DateInput name="date" Icon={Cake} />;
+export const Default = (): JSX.Element => (
+  <Formik initialValues={{}} onSubmit={() => {}}>
+    <DateInput name="date" />
+  </Formik>
+);
+export const WithIcon = (): JSX.Element => (
+  <Formik initialValues={{}} onSubmit={() => {}}>
+    <DateInput name="date" Icon={Cake} />
+  </Formik>
+);
+const error = {
+  date: "invalid",
+};
+export const WithError = (): JSX.Element => (
+  <Formik initialValues={{}} initialErrors={error} onSubmit={() => {}}>
+    <DateInput name="date" Icon={Cake} />
+  </Formik>
+);
