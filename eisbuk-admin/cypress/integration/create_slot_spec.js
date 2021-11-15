0f08607c9be518788145871335fd130751d5c186
fadwa <fadwa.mahmoud@gmail.com>
2021-11-16 00:20:29 +0200

    Test various scenarios for creating slots

--- eisbuk-admin/cypress/integration/create_slot_spec.js
+++ eisbuk-admin/cypress/integration/create_slot_spec.js
@@ -7,30 +7,69 @@ beforeEach(() => {
   });
   cy.visit(`/debug`);
 
-  cy.contains("Create admin test users").click();
   cy.intercept("POST", "/eisbuk/europe-west6/createOrganization").as(
     "createOrganization"
   );
-
   cy.intercept(
     "POST",
     "www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=fake-key"
   ).as("signupNewUser");
 
+  cy.contains("Create admin test users").click();
+
   cy.wait("@createOrganization").then(({ request }) => {
     expect(request.body).to.have.property("data");
   });
   cy.wait("@signupNewUser").then(({ request }) => {
-    expect(request.body).to.have.property("email", "test@eisbuk.it");
+    expect(request.body).to.have.property("email", `test@eisbuk.it`);
   });
 });
+
 describe("Create slot", () => {
   it("fills in slot form and submits it", () => {
     cy.visit("/prenotazioni");
     cy.get("[type='checkbox']").click();
     cy.get("[data-testid='new-slot-button']").first().click();
     cy.get("[value='ice']").check();
+    cy.get("[value='competitive']").check();
+    cy.get("[type='text']").eq(0).clear().type("09:00");
+    cy.get("[type='text']").eq(1).clear().type("10:30");
+    cy.get("[name='notes']").type("some notes");
+    cy.get("[type='submit']").click();
+
+    //check for created slot or snackbar
 
     // tst for disabled checkbox
+  });
+  it("creates an off-ice slot", () => {
+    cy.visit("/prenotazioni");
+    cy.get("[type='checkbox']").click();
+    cy.get("[data-testid='new-slot-button']").first().click();
+    cy.get("[value='off-ice-dancing']").check();
+
+    //check for value of the entire checkbox to be disabled
+    cy.get("[value='competitive']").should("be.disabled");
+
+    cy.get("[type='text']").eq(0).clear().type("09:00");
+    cy.get("[type='text']").eq(1).clear().type("10:30");
+    cy.get("[name='notes']").type("some notes");
+    cy.get("[type='submit']").click();
+  });
+  it("creates a multi-interval slot", () => {
+    cy.visit("/prenotazioni");
+    cy.get("[type='checkbox']").click();
+    cy.get("[data-testid='new-slot-button']").first().click();
+    cy.get("[value='ice']").check();
+    cy.get("[value='competitive']").check();
+
+    cy.get("[data-testid='add-interval']").click();
+
+    cy.get("[type='text']").eq(2).clear().type("09:00");
+    cy.get("[type='text']").eq(3).clear().type("10:30");
+    cy.get("[data-testid='add-interval']").click();
+    cy.get("[name='notes']").type("some notes");
+    cy.get("[type='text']").eq(2).clear().type("11:00");
+    cy.get("[type='text']").eq(3).clear().type("12:30");
+    cy.get("[type='submit']").click();
   });
 });
