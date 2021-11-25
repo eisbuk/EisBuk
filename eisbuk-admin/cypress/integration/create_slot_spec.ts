63311cd2354a2bde0fdf479c39f4e958052ceaf1
Silvio Tomatis <silviot@gmail.com>
2021-11-25 12:55:36 +0100

    Add tests for invalid time format and invalid interval

--- eisbuk-admin/cypress/integration/create_slot_spec.ts
+++ eisbuk-admin/cypress/integration/create_slot_spec.ts
@@ -13,12 +13,12 @@ beforeEach(() => {
 
 describe("Create slot", () => {
   beforeEach(() => {
-    cy.visit("/");
+    cy.visit(PrivateRoutes.Root);
 
     cy.get("div[aria-label='Page Navigation']").as("Page-Nav");
 
     cy.get("@Page-Nav").within(() => {
-      cy.get("a[href='/']")
+      cy.get(`a[href='${PrivateRoutes.Root}']`)
         .should("have.attr", "aria-disabled", "true")
         .and("have.attr", "aria-current", "page");
 
@@ -91,5 +91,36 @@ describe("Create slot", () => {
 
     cy.get("[aria-label='Additional slot notes']").type("some notes");
     cy.get("[aria-label='Confirm slot creation']").click();
+  });
+
+  it("submits an invalid time format", () => {
+    cy.visit(PrivateRoutes.Slots);
+    cy.get("[type='checkbox']").click();
+    cy.get("[data-testid='new-slot-button']").first().click();
+    cy.get("[value='ice']").check();
+    cy.get("[value='competitive']").check();
+
+    cy.get("[type='text']").eq(0).clear().type("9");
+    cy.get("[type='text']").eq(1).clear().type("10 pm");
+
+    cy.get("[type='submit']").click();
+    cy.contains("Invalid time format");
+  });
+  it("submits an invalid interval", () => {
+    cy.visit(PrivateRoutes.Slots);
+    cy.get("[type='checkbox']").click();
+    cy.get("[data-testid='new-slot-button']").first().click();
+    cy.get("[value='ice']").check();
+    cy.get("[value='competitive']").check();
+
+    cy.get("[type='text']").eq(0).clear().type("9:00");
+    cy.get("[type='text']").eq(1).clear().type("7:00");
+
+    cy.get("[type='submit']").click();
+    // The dialog is still there
+    cy.get("[role='dialog'").within(() => {
+      // and it contains this error message
+      cy.contains("Start time is greater than end time");
+    });
   });
 });
