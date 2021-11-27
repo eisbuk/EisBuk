a5f4eb363669cf8c96581760c8a14717d483fd66
ikusteu <ivan.kust57@gmail.com>
2021-11-27 11:54:15 +0100

    Switch build process to use entirely ESBuild, TODO: debug serve, add ESBuild compilation of build scripts

--- eisbuk-admin/esbuild/src/build.ts
+++ eisbuk-admin/esbuild/src/build.ts
@@ -2,9 +2,10 @@ import { build } from "esbuild";
 import path from "path";
 import fs from "fs";
 
-import { loadEnv } from "./lib/utils";
-
 import config from "./lib/config";
+import loadEnv from "./lib/loadEnv";
+import loadNodeArgs from "./lib/loadNodeArgs";
+import { createLogger } from "./lib/utils";
 
 const publicPath = path.join(process.cwd(), "public");
 const distPath = path.join(process.cwd(), "dist");
@@ -14,9 +15,40 @@ const distPath = path.join(process.cwd(), "dist");
  * with provided config.
  */
 const buildApp = async () => {
-  // load env vars to be bundled in the code
-  const processEnv = await loadEnv(process.cwd(), "production");
+  const logger = createLogger("BUILD_APP");
 
+  const args = loadNodeArgs();
+
+  // check nodeEnv and fallback to "development" if needed
+  let NODE_ENV = "development";
+  const nodeEnvWhitelist = ["development", "test", "storybook", "production"];
+
+  const nodeEnvInvalid =
+    typeof args.mode !== "string" || !nodeEnvWhitelist.includes(args.mode);
+
+  if (!args.mode) {
+    logger.log(`NODE_ENV not specified, using "${NODE_ENV}" as default`);
+  } else if (nodeEnvInvalid) {
+    logger.log(`Invalid value for NODE_ENV, using "${NODE_ENV}" as default`);
+  } else {
+    NODE_ENV = args.mode;
+    logger.log(`Using provided value for NODE_ENV: "${NODE_ENV}"`);
+  }
+
+  // check for env variable prefix and fall back to "REACT_APP" if not defined
+  let envPrefix = "REACT_APP";
+  if (!args.envPrefix) {
+    logger.log(`No --env-prefix specified, falling back to ${envPrefix}`);
+  } else {
+    envPrefix = args.envPrefix;
+  }
+
+  // load env vars to be bundled in the code
+  logger.log(`Loading env variables in mode "${NODE_ENV}"`);
+  const processEnv = await loadEnv(process.cwd(), NODE_ENV, envPrefix);
+
+  // bundle the app
+  logger.log("Creating an optimized production build...");
   build({
     ...config,
     define: { process: processEnv },
@@ -26,22 +58,34 @@ const buildApp = async () => {
     sourcemap: true,
     metafile: true,
   });
+
+  logger.log("Build process successfully finished");
 };
 
 /**
  * Uses (async) `fs.copyFile` to copy the contents of `/public` folder
  * to the `/dist` folder, ready for deployment
  */
-const copyPublicToDist = async () => {
+const copyPublicFiles = async () => {
+  const logger = createLogger("COPY_PUBLIC_FILES");
+
+  logger.log(`Looking for public files in ${publicPath}`);
   const pFiles = fs.readdirSync(publicPath);
+  logger.log("/public folder found");
+  logger.log(`Copying public files to ${distPath}`);
 
   // create a `/dist` folder if one doesn't already exist
   try {
-    fs.mkdir(distPath, () => {
-      console.log(`No ${distPath} folder found, created new`);
-    });
+    // convert `fs.mkdir` to promise to prevent race condition
+    // with single file copy/paste flow
+    await new Promise<void>((res) =>
+      fs.mkdir(distPath, () => {
+        logger.log(`No ${distPath} folder found, created new`);
+        res();
+      })
+    );
   } catch (err) {
-    console.error(err);
+    logger.error(err);
   }
 
   // copy all files from `/public` to `/dist` folder
@@ -49,10 +93,16 @@ const copyPublicToDist = async () => {
     pFiles.map((fName) => {
       const pathToFile = path.join(publicPath, fName);
       const distFilePath = path.join(distPath, fName);
-      return fs.copyFile(pathToFile, distFilePath, (err) => {
-        if (err) throw err;
-        console.log(`Copied ${pathToFile} to '/dist' folder for deployment`);
-      });
+
+      // return promise resolved inside `fs.copyFile` callback
+      // in order to be able to await on resolution
+      return new Promise<void>((res) =>
+        fs.copyFile(pathToFile, distFilePath, (err) => {
+          if (err) throw err;
+          logger.log(`Copied ${pathToFile} to '/dist' folder for deployment`);
+          res();
+        })
+      );
     })
   );
 };
@@ -61,5 +111,6 @@ const copyPublicToDist = async () => {
  * Main execution
  */
 (async () => {
-  await Promise.all([buildApp(), copyPublicToDist()]);
+  await Promise.all([buildApp(), copyPublicFiles()]);
+  createLogger("BUILD").log("Build process successfully finished");
 })();
--- eisbuk-admin/esbuild/src/lib/utils.ts
+++ eisbuk-admin/esbuild/src/lib/utils.ts
@@ -1,55 +1,53 @@
-import { parse as parseEnv } from "dotenv";
-import path from "path";
-import fs from "fs";
+interface EConsole {
+  log: Console["log"];
+  warn: Console["warn"];
+  error: Console["error"];
+}
 
 /**
- * Searches for `.env` file and `.env.${NODE_ENV}.local` files.
- * If env file(s) found, returns a stringified object populated with env variables (including `NODE_ENV`),
- * to be used as `process.env`, if files are empty/not-found, the return object contains only `NODE_ENV`.
- *
- * @param {string} rootPath path to the root of the project, where to look for the .env files
- * @param {string} nodeEnv `"development"`, `"test"` or `"production"`, if not specified (or invalid), falls back to "development"
- * @returns JSON stringified object to be used as process, containing `{ env: { ...envVariables } }`
+ * Creates a custom logger with `prefix` prepended to each message.
+ * The created logger supports `.log`, `.warn` and `.error` methods.
+ * @param prefix a prefix which to prepend to each logged string
+ * @returns
  */
-export const loadEnv = async (
-  rootPath: string,
-  nodeEnv: string
-): Promise<string> => {
-  const nodeEnvWhitelist = ["development", "test", "production"];
+export const createLogger = (prefix: string): EConsole => ({
+  log: (message, ...additional) =>
+    console.log(`[${prefix}] ${message}`, ...additional),
+  warn: (message, ...additional) =>
+    console.warn(`[${prefix}] ${message}`, ...additional),
+  error: (message, ...additional) =>
+    console.error(`[${prefix}] ${message}`, ...additional),
+});
 
-  const nodeEnvInvalid =
-    typeof nodeEnv !== "string" || !nodeEnvWhitelist.includes(nodeEnv);
+/**
+ * Picks the entries from passed record with
+ * keys starting with provided prefix
+ * @param record record to filter
+ * @param prefix of the keys picked for return value
+ * @returns record containing only entries keyed by strings with provided prefix
+ */
+export const pickWithPrefix = (
+  record: Record<string, any>,
+  prefix: string
+): Record<string, any> => {
+  if (!prefix) throw new Error("[PICK_WITH_PREFIX] Prefix is not defined");
 
-  let NODE_ENV;
-
-  if (!nodeEnv) {
-    NODE_ENV = "development";
-    console.log('NODE_ENV not specified, using "development" as default');
-  } else if (nodeEnvInvalid) {
-    NODE_ENV = "development";
-    console.log('Invalid value for NODE_ENV, using "development" as default');
-  } else {
-    NODE_ENV = nodeEnv;
-    console.log(`Using provided value for NODE_ENV: "${NODE_ENV}"`);
-  }
-
-  let env = { NODE_ENV };
-
-  // the env variables are loaded in this order so that specific `.env.${NODE_ENV}.local`
-  // file can take presedence (overwrite vars present in both of the files)
-  [".env", `.env.${NODE_ENV}.local`].forEach((fName) => {
-    const pathToFile = path.join(rootPath, fName);
-
-    try {
-      // load env file
-      const envFile = fs.readFileSync(pathToFile);
-      // parse env file to extract vars as object
-      const envVars = parseEnv(envFile);
-      env = { ...env, ...envVars };
-    } catch {
-      //
-    }
-  });
-
-  return JSON.stringify({ env });
+  return Object.keys(record).reduce(
+    (acc, key) =>
+      key.startsWith(prefix) ? { ...acc, [key]: record[key] } : acc,
+    {} as Record<string, any>
+  );
 };
+
+/**
+ * Replaces kebab-case string with camelCase
+ *
+ * ```
+ * kebabToCamel("some-string")
+ * // "someString"
+ * ```
+ * @param str kabak-case string
+ * @returns camelCase string
+ */
+export const kebabToCamel = (str: string): string =>
+  str[0] + str.slice(1).replace(/-[a-z]/gi, (s) => s[1].toUpperCase());
