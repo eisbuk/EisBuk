384787e3acf42b7120491a1970c8a3ef670c2d96
Fadwa <fadwa.mahmoud@gmail.com>
2021-11-30 12:19:45 +0200

    Invalid date format cypress test cases

--- eisbuk-admin/cypress/integration/athlete_form_spec.ts
+++ eisbuk-admin/cypress/integration/athlete_form_spec.ts
@@ -29,4 +29,43 @@ describe("add athlete", () => {
     cy.getAttrWith("type", "submit").click();
     cy.contains(`${saul.name} ${saul.surname} update`);
   });
+  it("should enter invalid date format and fail to submit form", () => {
+    cy.visit(PrivateRoutes.Athletes);
+    cy.getAttrWith("data-testid", "add-athlete").click();
+    cy.getAttrWith("name", "name").type(saul.name);
+    cy.getAttrWith("name", "surname").type(saul.surname);
+    cy.getAttrWith("name", "email").type(saul.email);
+    cy.getAttrWith("name", "phone").type(saul.phone);
+    cy.getAttrWith("placeholder", "dd/mm/yyyy").first().type("12 nov 2021");
+    cy.getAttrWith("value", "competitive").check();
+    cy.getAttrWith("placeholder", "dd/mm/yyyy")
+      .eq(1)
+      .type(saul.certificateExpiration);
+    cy.getAttrWith("placeholder", "dd/mm/yyyy")
+      .eq(2)
+      .type(saul.covidCertificateReleaseDate);
+    cy.getAttrWith("type", "checkbox").check();
+    cy.getAttrWith("type", "submit").click();
+    cy.contains(`Invalid date format ("dd/mm/yyyy")`);
+  });
+  it("should enter date separated with - and check that it's modified to /", () => {
+    cy.visit(PrivateRoutes.Athletes);
+    cy.getAttrWith("data-testid", "add-athlete").click();
+    cy.getAttrWith("name", "name").type(saul.name);
+    cy.getAttrWith("name", "surname").type(saul.surname);
+    cy.getAttrWith("name", "email").type(saul.email);
+    cy.getAttrWith("name", "phone").type(saul.phone);
+    cy.getAttrWith("placeholder", "dd/mm/yyyy").first().type("12-12-1990");
+    cy.getAttrWith("value", "competitive").check();
+    cy.getAttrWith("placeholder", "dd/mm/yyyy")
+      .eq(1)
+      .type(saul.certificateExpiration);
+    cy.getAttrWith("placeholder", "dd/mm/yyyy")
+      .eq(2)
+      .type(saul.covidCertificateReleaseDate);
+    cy.getAttrWith("type", "checkbox").check();
+    cy.getAttrWith("value", "12/12/1990");
+
+    cy.getAttrWith("type", "submit").click();
+  });
 });
