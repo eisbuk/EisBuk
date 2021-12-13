28184499f73ef450ab6a5906feb857f1cdaf1296
Fadwa <fadwa.mahmoud@gmail.com>
2021-12-13 11:54:33 +0200

    Modify customers birthday selector tests

--- eisbuk-admin/src/store/selectors/__tests__/customers.test.ts
+++ eisbuk-admin/src/store/selectors/__tests__/customers.test.ts
@@ -5,11 +5,12 @@ import {
   expectedCustomersBirthdays,
 } from "../__testData__/customers";
 import { getCustomersWithBirthday } from "../customers";
+import { DateTime } from "luxon";
 
 describe("Customer Selectors", () => {
   describe("Customers birthdays", () => {
     test("should get customers sorted according to their birthday", () => {
-      const selector = getCustomersWithBirthday;
+      const selector = getCustomersWithBirthday(DateTime.now());
       // test created selector against test store state
       const res = selector(testStore as LocalStore);
       expect(res).toEqual(expectedCustomersBirthdays);
