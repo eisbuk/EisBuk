161044da4b94ec2c232b3db28559f5eeb79153a8
Fadwa <fadwa.mahmoud@gmail.com>
2021-12-25 19:59:13 +0200

    Move birthday inside BirthdayMenuItem

--- eisbuk-admin/src/components/atoms/BirthdayMenu/BirthdayMenuItem.tsx
+++ eisbuk-admin/src/components/atoms/BirthdayMenu/BirthdayMenuItem.tsx
@@ -3,14 +3,31 @@ import React from "react";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import Box from "@material-ui/core/Box";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 import { Customer } from "eisbuk-shared";
 
 import EisbukAvatar from "@/components/users/EisbukAvatar";
+import { DateTime } from "luxon";
+import { DateFormat } from "@/enums/translations";
 
-type Props = Customer;
+import { useTranslation } from "react-i18next";
 
-const BirthdayMenuItem: React.FC<Props> = ({ ...customer }) => {
+interface Props {
+  customer: Customer;
+  showAll?: boolean;
+}
+
+const BirthdayMenuItem: React.FC<Props> = ({ customer, showAll = false }) => {
+  const classes = useStyles();
+  const { t } = useTranslation();
+
+  const customerBirthday =
+    customer.birthday === DateTime.now().toISODate().substring(5)
+      ? t("Today")
+      : t(DateFormat.DayMonth, {
+          date: DateTime.fromISO(customer.birthday || ""),
+        });
   return (
     <TableRow>
       <TableCell>
@@ -18,11 +35,26 @@ const BirthdayMenuItem: React.FC<Props> = ({ ...customer }) => {
           <EisbukAvatar {...customer} wrap={false} />
         </Box>
       </TableCell>
-      <TableCell>
+      <TableCell className={classes.fullWidthCell}>
         {customer.name} {customer.surname}
+        {!showAll && <div className={classes.birthday}>{customerBirthday}</div>}
       </TableCell>
     </TableRow>
   );
 };
 
 export default BirthdayMenuItem;
+
+// #region styles
+const useStyles = makeStyles(() => ({
+  birthday: {
+    position: "absolute",
+    transform: "translateY(-100%)",
+    right: "1rem",
+    fontWeight: "bold",
+  },
+  fullWidthCell: {
+    width: "100%",
+  },
+}));
+// #endregion styles
