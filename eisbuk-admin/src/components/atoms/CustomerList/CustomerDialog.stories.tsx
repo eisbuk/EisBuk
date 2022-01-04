a8eaeab880661b71dddbb939ee59f785828d0ef9
Fadwa <fadwa.mahmoud@gmail.com>
2022-01-04 13:05:23 +0200

    CustomerDialog component for athlete details

---
+++ eisbuk-admin/src/components/atoms/CustomerList/CustomerDialog.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ComponentMeta } from "@storybook/react";
+
+import CustomerDialog from "./CustomerDialog";
+
+import { walt } from "@/__testData__/customers";
+
+export default {
+  title: "Customer Dialog",
+  component: CustomerDialog,
+} as ComponentMeta<typeof CustomerDialog>;
+
+// const customers = Object.values(customersObj);
+
+export const Default = (): JSX.Element => (
+  <CustomerDialog customer={walt} open={true} onClose={(): void => {}} />
+);
