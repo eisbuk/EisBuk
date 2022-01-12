1cfb99e6d2ef3f889db08b1f48bc9bc6c4a4b677
Fadwa <fadwa.mahmoud@gmail.com>
2022-01-12 16:29:32 +0200

    All customer info

--- eisbuk-admin/src/components/atoms/CustomerList/CustomerDialog.tsx
+++ eisbuk-admin/src/components/atoms/CustomerList/CustomerDialog.tsx
@@ -3,7 +3,6 @@ import { useHistory } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 
-import IconButton from "@material-ui/core/IconButton";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
@@ -21,7 +20,12 @@ import Mail from "@material-ui/icons/Mail";
 import { Customer } from "eisbuk-shared";
 
 import { Routes } from "@/enums/routes";
-import { CategoryLabel, CustomerLabel, Prompt } from "@/enums/translations";
+import {
+  CategoryLabel,
+  CustomerActionButtons,
+  CustomerLabel,
+  Prompt,
+} from "@/enums/translations";
 
 import EisbukAvatar from "@/components/users/EisbukAvatar";
 import ConfirmDialog from "@/components/global/ConfirmDialog";
@@ -41,6 +45,7 @@ import {
   __sendBookingsEmailId__,
 } from "./__testData__/testIds";
 import { __customersDialogId__ } from "@/__testData__/testIds";
+import Button from "@material-ui/core/Button";
 
 interface Props {
   customer: Customer | null;
@@ -58,26 +63,70 @@ const CustomerDialog: React.FC<Props> = ({ onClose, customer }) => {
       open={Boolean(customer)}
       onClose={onClose}
     >
-      <Card className={classes.Card}>
+      <Card>
         <CardContent className={classes.CardContent}>
           <Box className={classes.Box}>
             <EisbukAvatar
               {...{ ...customer!, className: classes.BiggerAvatar }}
             />
-            <Typography variant="h4" className={classes.Name}>
-              {customer?.name} {customer?.surname}
-            </Typography>
+            <div className={classes.NameContainer}>
+              <Typography variant="h4" className={classes.Name}>
+                {customer?.name}
+              </Typography>
+              <Typography variant="h4" className={classes.Name}>
+                {customer?.surname}
+              </Typography>
+            </div>
           </Box>
-
           <br />
           <Typography variant="h6" component="div">
-            {t(CustomerLabel.Category)}:{" "}
+            <span className={classes.BoldLabel}>
+              {t(CustomerLabel.Category)}:{" "}
+            </span>
             {capitalizeFirst(
               t(CategoryLabel[customer?.category || ""])
             ).replace("-", " ")}
           </Typography>
           <Typography variant="h6">
-            {t(CustomerLabel.Email)}: {customer?.email}
+            <span className={classes.BoldLabel}>
+              {t(CustomerLabel.Email)}:{" "}
+            </span>
+            {customer?.email}
+          </Typography>
+          <Typography variant="h6">
+            <span className={classes.BoldLabel}>
+              {" "}
+              {t(CustomerLabel.DateOfBirth)}:{" "}
+            </span>
+            {customer?.birthday}
+          </Typography>
+          <Typography variant="h6">
+            <span className={classes.BoldLabel}>
+              {" "}
+              {t(CustomerLabel.Phone)}:{" "}
+            </span>
+            {customer?.phone}
+          </Typography>
+          <Typography variant="h6">
+            <span className={classes.BoldLabel}>
+              {" "}
+              {t(CustomerLabel.CardNumber)}:{" "}
+            </span>
+            {customer?.subscriptionNumber || "-"}
+          </Typography>
+          <Typography variant="h6">
+            <span className={classes.BoldLabel}>
+              {" "}
+              {t(CustomerLabel.MedicalCertificate)}:{" "}
+            </span>
+            {customer?.certificateExpiration}
+          </Typography>
+          <Typography variant="h6">
+            <span className={classes.BoldLabel}>
+              {" "}
+              {t(CustomerLabel.CovidCertificateReleaseDate)}:{" "}
+            </span>
+            {customer?.covidCertificateReleaseDate}
           </Typography>
         </CardContent>
         <CardActions className={classes.CardActions}>
@@ -134,37 +183,45 @@ const ActionButtons: React.FC<Omit<Props, "open">> = ({
 
   return (
     <>
-      <IconButton
+      <Button
         color="primary"
         onClick={() => setDeleteDialog(true)}
         data-testid={__customerDeleteId__}
+        variant="contained"
       >
-        <DeleteIcon />
-      </IconButton>
-      <IconButton
+        <DeleteIcon style={{ paddingRight: "5px" }} />
+        {t(CustomerActionButtons.Delete)}
+      </Button>
+      <Button
         aria-label="edit"
         color="primary"
         onClick={openCustomerForm}
         data-testid={__customerEditId__}
+        variant="contained"
       >
-        <EditIcon />
-      </IconButton>
-      <IconButton
+        <EditIcon style={{ paddingRight: "5px" }} />
+        {t(CustomerActionButtons.Edit)}
+      </Button>
+      <Button
         color="primary"
         onClick={redirectToBookings}
         data-testid={__openBookingsId__}
+        variant="contained"
       >
-        <DateRangeIcon />
-      </IconButton>
-      <IconButton
+        <DateRangeIcon style={{ paddingRight: "5px" }} />
+        {t(CustomerActionButtons.Bookings)}
+      </Button>
+      <Button
         color="primary"
         onClick={() => setSendMailDialog(true)}
         data-testid={__sendBookingsEmailId__}
+        variant="contained"
         // disable button if email or secret key not provided
         disabled={!(customer?.email && customer?.secretKey)}
       >
-        <Mail />
-      </IconButton>
+        <Mail style={{ paddingRight: "5px" }} />
+        {t(CustomerActionButtons.SendEmail)}
+      </Button>
 
       <ConfirmDialog
         open={deleteDialog}
@@ -194,22 +251,17 @@ const ActionButtons: React.FC<Omit<Props, "open">> = ({
 
 const useStyles = makeStyles(() => ({
   CursorPointer: { cursor: "pointer" },
-  Card: {
-    width: "35rem",
-    // padding: "1rem",
-  },
   CardActions: {
     justifyContent: "center",
   },
   CardContent: {
+    width: "40rem",
     display: "flex",
     flexDirection: "column",
   },
   BiggerAvatar: {
     width: "5rem",
     height: "5rem",
-    // marginRight: "1rem",
-    // borderRadius: "100%",
   },
   Name: {
     paddingLeft: "1.5rem",
@@ -219,6 +271,13 @@ const useStyles = makeStyles(() => ({
     flexDirection: "row",
     alignItems: "center",
     marginBottom: "1rem",
+  },
+  BoldLabel: {
+    fontWeight: "bold",
+  },
+  NameContainer: {
+    display: "flex",
+    flexDirection: "column",
   },
 }));
 
