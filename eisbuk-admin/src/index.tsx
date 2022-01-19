186012088b4640398577cfdec8e080b3a8dec720
Silvio Tomatis <silviot@gmail.com>
2022-01-19 21:15:04 +0100

    Configure sentry BrowserTracing to trace location changes

--- eisbuk-admin/src/index.tsx
+++ eisbuk-admin/src/index.tsx
@@ -14,7 +14,22 @@ if (__sentryDSN__) {
   // init crashlytics only if dsn provided
   Sentry.init({
     dsn: __sentryDSN__,
-    integrations: [new Integrations.BrowserTracing()],
+    integrations: [
+      new Integrations.BrowserTracing({
+        beforeNavigate: (context) => {
+          return {
+            ...context,
+            name: location.pathname
+              .replace(
+                /\/[a-f0-9]{8}(-[a-f0-9]{4}){3}-[a-f0-9]{12}/g,
+                "/<secret-key>"
+              )
+              .replace(/\/[a-f0-9]{32}/g, "/<hash>")
+              .replace(/\/\d+/g, "/<digits>"),
+          };
+        },
+      }),
+    ],
     tracesSampleRate: 1.0,
   });
   console.log("Sentry initialized");
