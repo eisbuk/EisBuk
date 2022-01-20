db346cf5c072cc71035f3b1a6ce0aca862310852
Fadwa <fadwa.mahmoud@gmail.com>
2022-01-21 00:21:29 +0200

    Rerender birthday menu on midnight

---
+++ eisbuk-admin/cypress/integration/birthday_menu_spec.ts
@@ -0,0 +1,48 @@
+import { PrivateRoutes } from "@/enums/routes";
+import { saul } from "@/__testData__/customers";
+import { __birthdayMenu__ } from "@/__testData__/testIds";
+import { DateTime } from "luxon";
+import { __storybookDate__ as __staticTestDate__ } from "@/lib/constants";
+
+describe("birthday badge", () => {
+  beforeEach(() => {
+    cy.initAdminApp();
+    cy.visit(PrivateRoutes.Athletes);
+    cy.clock(DateTime.fromISO(__staticTestDate__).toMillis());
+    cy.getAttrWith("data-testid", "add-athlete").click();
+    cy.getAttrWith("name", "name").type(saul.name);
+    cy.getAttrWith("name", "surname").type(saul.surname);
+    cy.getAttrWith("name", "email").type(saul.email);
+    cy.getAttrWith("name", "phone").type(saul.phone);
+    cy.getAttrWith("placeholder", "dd/mm/yyyy")
+      .first()
+      .type(DateTime.fromISO(__staticTestDate__).plus({ day: 1 }).toISODate());
+    cy.getAttrWith("value", "competitive").check();
+    cy.getAttrWith("placeholder", "dd/mm/yyyy")
+      .eq(1)
+      .type(saul.certificateExpiration);
+    cy.getAttrWith("placeholder", "dd/mm/yyyy")
+      .eq(2)
+      .type(saul.covidCertificateReleaseDate);
+    cy.getAttrWith("type", "checkbox").check();
+    cy.getAttrWith("type", "submit").click();
+    cy.contains(`${saul.name} ${saul.surname} update`);
+  });
+  it("should check for birthday menu rerendering on midnight", () => {
+    const time =
+      DateTime.fromISO(__staticTestDate__)
+        .plus({ day: 1 })
+        .startOf("day")
+        .toMillis() - DateTime.fromISO(__staticTestDate__).toMillis();
+    cy.visit(PrivateRoutes.Root);
+    cy.getAttrWith("data-testid", __birthdayMenu__)
+      .children()
+      .eq(1)
+      .should("have.text", 0);
+    cy.tick(time);
+    cy.getAttrWith("data-testid", __birthdayMenu__)
+      .children()
+      .eq(1)
+      .should("have.text", 1);
+  });
+});
