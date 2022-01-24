ec44f97c194cd5b0c07e9e174b8b5805cc269102
Fadwa <fadwa.mahmoud@gmail.com>
2022-01-24 17:06:32 +0200

    Use seeded customers

--- eisbuk-admin/cypress/integration/birthday_menu_spec.ts
+++ eisbuk-admin/cypress/integration/birthday_menu_spec.ts
@@ -1,31 +1,13 @@
 import { PrivateRoutes } from "@/enums/routes";
-import { saul } from "@/__testData__/customers";
 import { __birthdayMenu__ } from "@/__testData__/testIds";
 import { DateTime } from "luxon";
 import { __storybookDate__ as __staticTestDate__ } from "@/lib/constants";
+import { getOrganization } from "@/lib/getters";
 
 describe("birthday badge", () => {
   beforeEach(() => {
     cy.initAdminApp();
-    cy.visit(PrivateRoutes.Athletes);
-    cy.getAttrWith("data-testid", "add-athlete").click();
-    cy.getAttrWith("name", "name").type(saul.name);
-    cy.getAttrWith("name", "surname").type(saul.surname);
-    cy.getAttrWith("name", "email").type(saul.email);
-    cy.getAttrWith("name", "phone").type(saul.phone);
-    cy.getAttrWith("placeholder", "dd/mm/yyyy")
-      .first()
-      .type(DateTime.fromISO(__staticTestDate__).plus({ day: 1 }).toISODate());
-    cy.getAttrWith("value", "competitive").check();
-    cy.getAttrWith("placeholder", "dd/mm/yyyy")
-      .eq(1)
-      .type(saul.certificateExpiration);
-    cy.getAttrWith("placeholder", "dd/mm/yyyy")
-      .eq(2)
-      .type(saul.covidCertificateReleaseDate);
-    cy.getAttrWith("type", "checkbox").check();
-    cy.getAttrWith("type", "submit").click();
-    cy.contains(`${saul.name} ${saul.surname} update`);
+    cy.updateFirestore(getOrganization(), ["customers.json"]);
   });
   it("should check for birthday menu rerendering on midnight", () => {
     const time =
