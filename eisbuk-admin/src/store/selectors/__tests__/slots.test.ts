3373405af691671bb838c2c29967fa0086965d4f
ikusteu <ivan.kust57@gmail.com>
2022-01-28 19:42:21 +0100

    Add tests for getAdminSlots and getSlotsForCustomer 'week' timeframe when passed non-week-start date

--- eisbuk-admin/src/store/selectors/__tests__/slots.test.ts
+++ eisbuk-admin/src/store/selectors/__tests__/slots.test.ts
@@ -44,6 +44,19 @@ describe("Slot selectors > ", () => {
       const res = selector(testStore as LocalStore);
       expect(res).toEqual(expectedWeekCustomer);
     });
+
+    test("should get slots for a week even if passed non-week-start-date", () => {
+      const testDate = DateTime.fromISO(currentWeekStartDate).plus({ days: 1 });
+      // create a selector curried with `timeframe` and `date` params (like we will be doing within the component)
+      const selector = getSlotsForCustomer(
+        Category.Competitive,
+        "week",
+        testDate
+      );
+      // test created selector against test store state
+      const res = selector(testStore as LocalStore);
+      expect(res).toEqual(expectedWeekCustomer);
+    });
   });
 
   describe("Test 'getAdminSlots' selector", () => {
@@ -55,6 +68,17 @@ describe("Slot selectors > ", () => {
     test("should return empty days if no slots in store ('slotsByDay' = null)", () => {
       const slotsForWeek = getAdminSlots(noSlotsStore);
       expect(slotsForWeek).toEqual(emptyWeek);
+    });
+
+    test("should get all slots for given week even if passed a non-week-start date", () => {
+      const slotsForWeek = getAdminSlots({
+        ...testStore,
+        app: {
+          ...testStore.app,
+          calendarDay: testStore.app.calendarDay.plus({ days: 1 }),
+        },
+      });
+      expect(slotsForWeek).toEqual(expectedWeekAdmin);
     });
   });
 });
