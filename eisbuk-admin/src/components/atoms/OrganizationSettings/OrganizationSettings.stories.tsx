31625bc79937a80e4bb6c304cbe8e4fff23d1aaa
Fadwa <fadwa.mahmoud@gmail.com>
2022-02-22 16:06:41 +0200

    Modify organization settings story

--- eisbuk-admin/src/components/atoms/OrganizationSettings/OrganizationSettings.stories.tsx
+++ eisbuk-admin/src/components/atoms/OrganizationSettings/OrganizationSettings.stories.tsx
@@ -9,15 +9,16 @@ export default {
 
 const organization = {
   admins: ["Walt", "Gus"],
-  name: "Los Pollos",
-  SMTPURI: "gusfring:password@smtpserver:8080",
-  EmailFrom: "gus@lospollos.me",
-  NameFrom: "Gustavo",
-  EmailTemplate:
+  organizationName: "Los Pollos",
+  smtpUri: "gusfring:password@smtpserver:8080",
+  emailFrom: "gus@lospollos.me",
+  emailSender: "Gustavo",
+  emailTemplate:
     "This is An Email Template and it needs to be longer than usual to test out multiline fields.",
-  SMSFrom: "Gus",
-  SMSTemplate:
-    "This is An SMS Template and it needs to be longer than usual to test out multiline fields.",
+  smsFrom: "Gus",
+  smsTemplate:
+    "This is An SMS Template and it needs to be even longer than usual to test out multiline fields.",
+  smsUrl: "sms url example",
 };
 export const Default = (): JSX.Element => (
   <OrganizationSettings organization={organization} />
