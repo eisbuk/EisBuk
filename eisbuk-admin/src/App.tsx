1b7b62c8714f2c45e17727308bd7ae2619869582
Fadwa <fadwa.mahmoud@gmail.com>
2022-02-25 00:59:35 +0200

    Comment out useStyles called before themeProvider

--- eisbuk-admin/src/App.tsx
+++ eisbuk-admin/src/App.tsx
@@ -8,7 +8,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import MuiPickersUtilsProvider from "@material-ui/pickers/MuiPickersUtilsProvider";
 import { ThemeProvider } from "@material-ui/core/styles";
 
-import makeStyles from "@material-ui/core/styles/makeStyles";
+// import makeStyles from "@material-ui/core/styles/makeStyles";
 
 import { store } from "@/store";
 
@@ -21,7 +21,7 @@ import useConnectAuthToStore from "@/react-redux-firebase/hooks/useConnectAuthTo
 import { currentTheme } from "@/themes";
 
 const App: React.FC = () => {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   // connect auth to store to recieve firebase SDK's auth updates
   // through redux store
@@ -31,7 +31,7 @@ const App: React.FC = () => {
     <ReduxProvider store={store}>
       <ThemeProvider theme={currentTheme}>
         <MuiPickersUtilsProvider utils={LuxonUtils}>
-          <SnackbarProvider className={classes.root} maxSnack={3}>
+          <SnackbarProvider maxSnack={3}>
             <Notifier />
             <CssBaseline />
             <AppContent />
@@ -43,22 +43,22 @@ const App: React.FC = () => {
 };
 
 // #region styles
-const useStyles = makeStyles((theme) => ({
-  root: {
-    margin: theme.spacing(0.75, 0),
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     margin: theme.spacing(0.75, 0),
 
-    /**
-     * This is, hopefully, temporary workaround to a
-     * bug in notistack that causes snackbars to be unclickable.
-     * Notistack v.1.0.10 fixes that bug, however,
-     * it only works with materialUI v^5, which is in pre-release currently.
-     * The bug is caused by the root class not having the 'pointerEvents'
-     * property set to 'all', 'root' class here overrides that.
-     * @TODO upgade MUI, notistack and remove this when MUI v5 becomes the LTS
-     */
-    pointerEvents: "all",
-  },
-}));
+/**
+ * This is, hopefully, temporary workaround to a
+ * bug in notistack that causes snackbars to be unclickable.
+ * Notistack v.1.0.10 fixes that bug, however,
+ * it only works with materialUI v^5, which is in pre-release currently.
+ * The bug is caused by the root class not having the 'pointerEvents'
+ * property set to 'all', 'root' class here overrides that.
+ * @TODO upgade MUI, notistack and remove this when MUI v5 becomes the LTS
+ */
+//     pointerEvents: "all",
+//   },
+// }));
 // #region styles
 
 export default App;
