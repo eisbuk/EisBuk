797cb6cbbdf73ca767c47f481cb3d0c2ae8743d1
Silvio Tomatis <silviot@gmail.com>
2022-03-01 21:26:58 +0100

    Add eisbuk-shared

---
+++ eisbuk-shared/src/enums/deprecated/firestore.ts
@@ -0,0 +1,23 @@
+export enum DeprecatedDuration {
+  "1h" = "60",
+  "1.5h" = "90",
+  "2h" = "120",
+}
+
+export enum DeprecatedOrgSubCollection {
+  Slots = "slots",
+  SlotsByDay = "slotsByDay",
+  Customers = "customers",
+  Bookings = "bookings",
+  BookingsByDay = "bookingsByDay",
+}
+
+export enum DeprecatedBookingSubCollection {
+  SubscribedSlots = "subscribedSlots",
+}
+
+export enum DeprecatedSlotType {
+  Ice = "ice",
+  OffIceDancing = "off-ice-dancing",
+  OffIceGym = "off-ice-gym",
+}
---
+++ eisbuk-shared/src/utils/customers.ts
@@ -0,0 +1,24 @@
+import { Customer, CustomerBase } from "../types/firestore";
+
+/**
+ * A helper function used to strip excess customer data
+ * and create customer base data (used as `bookings` data for customer)
+ * @param customer customer entry (without `secretKey` for convenient testing)
+ * @returns customer base structure
+ */
+export const getCustomerBase = ({
+  id,
+  name,
+  surname,
+  category,
+  extendedDate,
+  deleted,
+}: Omit<Customer, "secretKey">): CustomerBase => ({
+  id,
+  name,
+  surname,
+  category,
+  // add extended date only if it exists, rather than saving `extendedDate: undefined`
+  ...(extendedDate ? { extendedDate } : {}),
+  deleted: Boolean(deleted),
+});
