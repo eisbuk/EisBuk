c25f258e2fcb25470aadbd13f8ac0cc5c40ab7cd
ikusteu <ivan.kust57@gmail.com>
2022-03-02 18:30:40 +0100

    Create styled AuthTextField

---
+++ eisbuk-admin/src/components/atoms/AuthDialog/AuthTextField.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FastField, FieldConfig, useField } from "formik";
+
+import { TextField, TextFieldProps } from "formik-material-ui";
+
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const AuthTextField: React.FC<
+  FieldConfig<string> & Pick<TextFieldProps, "label">
+> = ({ name, ...props }) => {
+  const classes = useStyles();
+
+  const [, { error }] = useField(name);
+
+  return (
+    <>
+      <div className={classes.container}>
+        <FastField
+          className={classes.textField}
+          {...{ name, ...props }}
+          component={TextField}
+          onBlur={() => {}}
+        />
+      </div>
+      <div>{error}</div>
+    </>
+  );
+};
+
+const useStyles = makeStyles(() => ({
+  container: {
+    padding: "20px 0",
+    width: "100%",
+  },
+  textField: {
+    width: "100%",
+  },
+  errorContainer: {
+    width: "100%",
+    height: "1rem",
+  },
+}));
+
+export default AuthTextField;
