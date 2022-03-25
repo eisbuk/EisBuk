304e3310ad80ec6a56b0be0c3db9eb7255d0accd
ikusteu <ivan.kust57@gmail.com>
2022-03-25 22:07:46 +0100

    Remove unnecessart esbuild dependency from client app

--- eisbuk-admin/.eslintrc.js
+++ eisbuk-admin/.eslintrc.js
@@ -14,12 +14,10 @@ module.exports = {
   root: true,
   ignorePatterns: [
     ...scaffold.ignorePatterns,
-    "dev-server-meta/" /** @TODO remove this when we switch to vite */,
     "coverage/",
     "setupTests.js",
     "nyc-config.js",
     "jest.config.js",
-    "esbuild/bundler.js",
   ],
   rules: {
     ...scaffold.rules,
