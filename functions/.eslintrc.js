10eaaa784c324efe77520b6972f586b74704e95e
ikusteu <ivan.kust57@gmail.com>
2022-03-26 10:30:12 +0100

    Fix lint-staged path resolution by explicitly stating tsConfig

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,24 +1,26 @@
-const scaffold = require("../scaffolding/.eslintrc.js");
 const path = require("path");
 
-module.exports = {
-  ...scaffold,
-  parserOptions: {
-    ...scaffold.parserOptions,
-    project: path.join(__dirname, "./tsconfig.json"),
+const scaffold = require("../scaffolding/.eslintrc.js");
+const { useTSConfig } = require("../scaffolding/.eslint.utils.js");
+
+const tsPaths = [path.join(__dirname, "./tsconfig.json")];
+
+module.exports = useTSConfig(
+  {
+    ...scaffold,
+    rules: {
+      ...scaffold.rules,
+
+      // Removed rule "disallow the use of console" from recommended eslint rules
+      "no-console": "warn",
+
+      // Enforces the use of catch() on un-returned promises
+      "promise/catch-or-return": 2,
+
+      // Warn against nested then() or catch() statements
+      "promise/no-nesting": 1,
+    },
+    ignorePatterns: [...scaffold.ignorePatterns, "build_scripts"],
   },
-  root: true,
-  rules: {
-    ...scaffold.rules,
-
-    // Removed rule "disallow the use of console" from recommended eslint rules
-    "no-console": "warn",
-
-    // Enforces the use of catch() on un-returned promises
-    "promise/catch-or-return": 2,
-
-    // Warn against nested then() or catch() statements
-    "promise/no-nesting": 1,
-  },
-  ignorePatterns: [...scaffold.ignorePatterns, "build_scripts"],
-};
+  tsPaths
+);
