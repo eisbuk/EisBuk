19c473cf678b9eb063611d34d92bbd09472f102a
Fadwa <fadwa.mahmoud@gmail.com>
2022-04-12 14:08:26 +0200

    Use renderWithRedux in AddToCalendar

--- packages/client/src/components/atoms/BookingsCountdown/__tests__/AddToCalendar.test.tsx
+++ packages/client/src/components/atoms/BookingsCountdown/__tests__/AddToCalendar.test.tsx
@@ -3,21 +3,20 @@
  */
 
 import React from "react";
-import { screen, render } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import * as reactRedux from "react-redux";
 
 import AddToCalendar from "../AddToCalendar";
 import { __addToCalendarButtonId__ } from "@/__testData__/testIds";
+import { renderWithRedux } from "@/__testUtils__/wrappers";
 
 const mockDispatch = jest.fn();
 jest.spyOn(reactRedux, "useDispatch").mockImplementation(() => mockDispatch);
-// mock createObjectURL
-global.URL.createObjectURL = jest.fn();
-// write a test for creating an event
+
 describe("Add To Calendar", () => {
   describe("Smoke Test", () => {
     test("should render component", () => {
-      render(<AddToCalendar />);
+      renderWithRedux(<AddToCalendar />);
       screen.getByTestId(__addToCalendarButtonId__);
     });
   });
