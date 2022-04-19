6259b3759fee2f69bf61feab86c1473ea7cbf0a6
Fadwa <fadwa.mahmoud@gmail.com>
2022-04-19 02:41:03 +0200

    Fix AddToCalendar test

--- packages/client/src/components/atoms/BookingsCountdown/__tests__/AddToCalendar.test.tsx
+++ packages/client/src/components/atoms/BookingsCountdown/__tests__/AddToCalendar.test.tsx
@@ -9,14 +9,18 @@ import * as reactRedux from "react-redux";
 import AddToCalendar from "../AddToCalendar";
 import { __addToCalendarButtonId__ } from "@/__testData__/testIds";
 import { renderWithRedux } from "@/__testUtils__/wrappers";
+import { bookedSlots } from "@/store/actions/__testData__/bookingOperations";
 
+jest.mock("react-router", () => ({
+  useParams: () => ({ secretKey: "secret-key" }),
+}));
 const mockDispatch = jest.fn();
 jest.spyOn(reactRedux, "useDispatch").mockImplementation(() => mockDispatch);
 
 describe("Add To Calendar", () => {
   describe("Smoke Test", () => {
     test("should render component", () => {
-      renderWithRedux(<AddToCalendar />);
+      renderWithRedux(<AddToCalendar bookedSlots={bookedSlots} />);
       screen.getByTestId(__addToCalendarButtonId__);
     });
   });
