ed301d51be028c54be7232cc08299ed620fe7cd5
delasous <sousa.yak@googlemail.com>
2022-04-20 11:40:55 +0200

    Add fn to retrieve subcollection paths: Lift ref to adminDb to global scope; Create customer & bookings subcollection data before each subcollection test.

--- packages/client/src/__tests__/download.test.ts
+++ packages/client/src/__tests__/download.test.ts
@@ -3,8 +3,13 @@
  */
 
 import { __testOrganization__ } from "@/__testSetup__/envData";
+import { adminDb } from "@/__testSetup__/firestoreSetup";
+import { deleteAll } from "@/__testUtils__/firestore";
+import { saul, walt } from "@/__testData__/customers";
 
-import { getOrgs } from "@eisbuk/firestore";
+import { OrgSubCollection, Collection } from "@eisbuk/shared";
+
+import { getOrgs, getSubCollectionPaths } from "@eisbuk/firestore";
 
 it("Lists all existing organizations", async () => {
   const result = await getOrgs();
@@ -22,4 +27,40 @@ it("Lists all existing organizations", async () => {
   } else {
     fail(result.message);
   }
+});
+
+describe("Collects subcollection facts", () => {
+  const customersSubcollectionPath = `${Collection.Organizations}/${__testOrganization__}/${OrgSubCollection.Customers}`;
+  const bookingsSubcollectionPath = `${Collection.Organizations}/${__testOrganization__}/${OrgSubCollection.Bookings}`;
+
+  beforeEach(async () => {
+    await adminDb.doc(`${customersSubcollectionPath}/${saul.id}`).set(saul);
+    await adminDb.doc(`${customersSubcollectionPath}/${walt.id}`).set(walt);
+
+    // * Note: `bookings` are created implicitly as a result of data trigger on Customers doc write
+  });
+
+  afterEach(async () => {
+    await deleteAll();
+  });
+
+  it("Returns an array of subcollection paths", async () => {
+    const paths = await getSubCollectionPaths(__testOrganization__);
+
+    const numOfSubcollections = paths.length;
+
+    const expectedPathsResult = [
+      {
+        id: "bookings",
+        path: bookingsSubcollectionPath,
+      },
+      {
+        id: "customers",
+        path: customersSubcollectionPath,
+      },
+    ];
+
+    expect(numOfSubcollections).toBe(2);
+    expect(paths).toEqual(expectedPathsResult);
+  });
 });
