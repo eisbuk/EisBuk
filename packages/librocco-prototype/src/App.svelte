a85bed3fbb2c6cef6e89d26158ae320b1641cad6
Silvio Tomatis <silviot@gmail.com>
2022-04-25 17:30:45 +0200

    Add librocco prototype (svelte/vite/ts/pouchdb)

---
+++ packages/librocco-prototype/src/App.svelte
@@ -0,0 +1,13 @@
+<main>
+  <h1>Hello Typescript!</h1>
+
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+    the officially supported framework, also powered by Vite!
+  </p>
+</main>
