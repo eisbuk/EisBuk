e45d8ee308086a4bb924aaddeb19f018e17fbae8
Silvio Tomatis <silviot@gmail.com>
2022-04-25 23:19:19 +0200

    Initialize a pouchdb database connected to a couchdb instance

--- packages/librocco-prototype/src/main.ts
+++ packages/librocco-prototype/src/main.ts
@@ -1,7 +1,27 @@
 import App from './App.svelte'
 
+import PouchDB from 'pouchdb';
+window.pouch_db = new PouchDB('http://localhost:5984/kittens');
+
+pouch_db.info().then(function (info) {
+    console.log(info);
+  })
+
+  var doc = {
+    "_id": "mittens",
+    "name": "Mittens",
+    "occupation": "kitten",
+    "age": 3,
+    "hobbies": [
+      "playing with balls of yarn",
+      "chasing laser pointers",
+      "lookin' hella cute"
+    ]
+};
+pouch_db.put(doc);
 const app = new App({
   target: document.getElementById('app')
 })
 console.log(app);
 export default app
+
--- packages/librocco-prototype/vite.config.ts
+++ packages/librocco-prototype/vite.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [
-    svelte({
-      /* plugin options */
-    })
-  ]
+  plugins: [svelte()]
 });
