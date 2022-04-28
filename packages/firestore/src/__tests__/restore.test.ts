bf3ceb3ac5dde1865482baca76a3efc342a2aa12
delasous <sousa.yak@googlemail.com>
2022-04-28 21:52:49 +0200

    Add fn to iterate over and write docs for a specified subcollection.

--- packages/firestore/src/__tests__/restore.test.ts
+++ packages/firestore/src/__tests__/restore.test.ts
@@ -1,17 +1,30 @@
 /**
  * @jest-environment node
  */
-import admin from "firebase-admin";
+import admin, { firestore } from "firebase-admin";
 
 import { adminDb } from "../__testSetup__/adminDb";
 import { deleteAll } from "../__testUtils__/deleteAll";
-import { defaultUser } from "../__testData__/customers";
+import { saul, walt, defaultUser } from "../__testData__/customers";
 
-import { Collection } from "@eisbuk/shared";
+import { OrgSubCollection, Collection } from "@eisbuk/shared";
 
 import * as restoreService from "../restore";
+import { ISubCollectionData } from "src/types";
 
 const __testOrganization__ = "test-organization-2";
+
+/**
+ * unPackCollectionData - Util to unwrap docs from query snapshot
+ */
+function unpackCollectionData(
+  querySnapshot: firestore.QuerySnapshot
+): ISubCollectionData[] {
+  const resultData: ISubCollectionData[] = [];
+  querySnapshot.forEach((doc) => resultData.push(doc.data()));
+
+  return resultData;
+}
 
 jest.spyOn(admin, "firestore").mockImplementation(() => adminDb);
 jest.spyOn(admin, "initializeApp").mockImplementation((() => {}) as any);
@@ -25,13 +38,13 @@ afterAll(() => {
 });
 
 describe("Restore service", () => {
-  it("Sets organization document data", async () => {
-    const orgId = __testOrganization__;
+  const orgPath = `${Collection.Organizations}/${__testOrganization__}`;
+
+  test("Sets organization document data", async () => {
     const orgData = { admins: [defaultUser.email] };
 
-    const orgPath = `${Collection.Organizations}/${orgId}`;
     const orgPayload = {
-      id: orgId,
+      id: __testOrganization__,
       data: orgData,
     };
 
@@ -45,5 +58,25 @@ describe("Restore service", () => {
     } else {
       throw new Error(res.message);
     }
+  });
+
+  test("Sets an array of individual docs in a specified subcollection", async () => {
+    const subColId = OrgSubCollection.Customers;
+    const subColPath = `${orgPath}/${subColId}`;
+    const subColData = {
+      saul: saul,
+      walt: walt,
+    };
+
+    await restoreService.setSubCollectionData(subColPath, subColData);
+
+    const result = await adminDb.collection(subColPath).get();
+
+    expect(result.empty).toBe(false);
+
+    const resultData = unpackCollectionData(result);
+    const expectedData = [saul, walt];
+
+    expect(resultData).toEqual(expectedData);
   });
 });
--- packages/firestore/src/restore.ts
+++ packages/firestore/src/restore.ts
@@ -1,7 +1,12 @@
 import admin from "firebase-admin";
 
 import { Collection } from "@eisbuk/shared";
-import { IOperationFailure, IOperationSuccess, IOrgRootData } from "./types";
+import {
+  IOperationFailure,
+  IOperationSuccess,
+  IOrgRootData,
+  ISubCollectionData,
+} from "./types";
 
 /**
  * setOrgRootData - Set organisation data
@@ -20,4 +25,27 @@ export async function setOrgRootData({
   } catch (err: any) {
     return { ok: false, message: err.message };
   }
+}
+
+/**
+ * setSubCollectionData - Set an array of individual docs in a specified subcollection
+ * @param path - SubCollection path: e.g Organisations/orgId/SubCollection
+ * @param data - SubCollection data obj: e.g { key = docId, value = data }
+ */
+export async function setSubCollectionData(
+  path: string,
+  data: ISubCollectionData
+): Promise<void> {
+  const db = admin.firestore();
+
+  const subDocumentDataArr = Object.entries(data);
+
+  const subDocWriteOps = subDocumentDataArr.map(async ([docId, docData]) => {
+    const docPath = `${path}/${docId}`;
+
+    return await db.doc(docPath).set(docData);
+  });
+
+  await Promise.all(subDocWriteOps);
+  return;
 }
