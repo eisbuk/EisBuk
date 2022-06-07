408c2256cfd9e22db9436d2974a1f0511184c8ac
ikusteu <ivan.kust57@gmail.com>
2022-06-07 19:35:04 +0200

    Apply packages/svg to TestComponent in packages/ui, udpate readme with SVG usage explanation

--- packages/ui/src/@types/assets/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "*.svg" {
-  import { SVGComponent } from "@eisbuk/shared";
-
-  const Component: SVGComponent;
-
-  export default Component;
-}
--- packages/ui/src/TestComponent/TestComponent.tsx
+++ packages/ui/src/TestComponent/TestComponent.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import SVG from "@assets/SVG.svg";
+import { TestSVG } from "@eisbuk/svg";
 
 const TestComponent: React.FC = () => {
   return (
     <div className="border-4 border-red-900 w-20 h-20">
-      <SVG />
+      <TestSVG />
     </div>
   );
 };
