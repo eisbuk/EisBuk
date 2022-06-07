f8f33a508aeb67fa66d3cdfc0ebe10f2d0826000
ikusteu <ivan.kust57@gmail.com>
2022-06-07 16:08:29 +0200

    Add TestComponent as to not fail the build and provide an example

---
+++ packages/ui/src/TestComponent/TestComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SVG from "@assets/SVG.svg";
+
+const TestComponent: React.FC = () => {
+  return (
+    <div className="border-4 border-red-900 w-20 h-20">
+      <SVG />
+    </div>
+  );
+};
+
+export default TestComponent;
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,6 @@
 // Main CSS is imported here to signal the bundler the usage of Tailwind
 // in order to generate a PostCSS built css file next to the bundle
 import "./main.css";
+
+import TestComponent from "./TestComponent";
+export { TestComponent };
