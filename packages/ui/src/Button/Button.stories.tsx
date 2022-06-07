e123baf525df80d6783614aff7ca8a58a929ac8d
ikusteu <ivan.kust57@gmail.com>
2022-06-07 20:21:33 +0200

    Scaffold Button component

---
+++ packages/ui/src/Button/Button.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ComponentMeta } from "@storybook/react";
+
+import Button from "./Button";
+
+export default {
+  title: "Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+export const Default = (): JSX.Element => <Button />;
---
+++ packages/ui/src/Button/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export type ButtonProps = React.HTMLAttributes<HTMLButtonElement>;
+
+const Button: React.FC<ButtonProps> = () => {
+  return null;
+};
+
+export default Button;
