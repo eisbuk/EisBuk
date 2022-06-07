f96e4a7089050e664e7af75cc8e2a65c9e5e5457
ikusteu <ivan.kust57@gmail.com>
2022-06-07 20:52:02 +0200

    Apply basic button styles

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -19,18 +19,51 @@ export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
 }
 
 const Button: React.FC<ButtonProps> = ({
+  size = ButtonSize.Base,
+  color,
   StartAdornment,
   EndAdornment,
   children,
+  className: classes,
   ...props
 }) => {
+  const className = [
+    ...baseClasses,
+    ...sizeClassesLookup[size],
+    ...(color ? colorClassesLookup[color] : []),
+    classes,
+  ].join(" ");
+
   return (
-    <button {...{ ...props }}>
-      {StartAdornment && <StartAdornment />}
+    <button {...{ ...props, className }}>
+      {StartAdornment && (
+        <div className="mr-1">
+          <StartAdornment />
+        </div>
+      )}
+
       {children}
-      {EndAdornment && <EndAdornment />}
+
+      {EndAdornment && (
+        <div className="ml-1">
+          <EndAdornment />
+        </div>
+      )}
     </button>
   );
+};
+
+const baseClasses = ["font-600", "rounded-md"];
+
+const sizeClassesLookup = {
+  [ButtonSize.Base]: ["px-[10px]", "py-[6px]", "text-sm"],
+  [ButtonSize.LG]: ["px-[17px]", "py-[9px]", "text-base"],
+};
+
+const colorClassesLookup = {
+  [ButtonColor.Primary]: ["bg-cyan-500"],
+  [ButtonColor.Secondary]: ["bg-yellow-600"],
+  [ButtonColor.Error]: ["bg-red-700"],
 };
 
 export default Button;
