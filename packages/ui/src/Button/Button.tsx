867aac11ce1af833359afb63a0d72fe28c9ab306
ikusteu <ivan.kust57@gmail.com>
2022-06-07 20:37:31 +0200

    Test click functionality

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 
-export type ButtonProps = React.HTMLAttributes<HTMLButtonElement>;
+export enum ButtonSize {
+  Base = "md",
+  LG = "lg",
+}
 
-const Button: React.FC<ButtonProps> = () => {
-  return null;
+export enum ButtonColor {
+  Primary = "primary",
+  Secondary = "secondary",
+  Error = "error",
+}
+
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  size?: ButtonSize;
+  color?: ButtonColor;
+  StartAdornment?: React.FC;
+  EndAdornment?: React.FC;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  StartAdornment,
+  EndAdornment,
+  children,
+  ...props
+}) => {
+  return (
+    <button {...{ ...props }}>
+      {StartAdornment && <StartAdornment />}
+      {children}
+      {EndAdornment && <EndAdornment />}
+    </button>
+  );
 };
 
 export default Button;
