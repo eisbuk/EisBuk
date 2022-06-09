0cc668fce9f228a7832660ac40f193beba15f739
Silvio Tomatis <silviot@gmail.com>
2022-06-09 23:39:07 +0200

    Ignore newlines when checking versions

--- .github/check-rush.sh
+++ .github/check-rush.sh
@@ -2,11 +2,20 @@
 
 echo Checking for newly inrtoduced dependency mismatches
 
-
+# remove newlines
 diff -w <(rush check --json |
     grep -A5000 -m1 -e '^$'|
     tail -n +5 |
-    jq -S .) .github/version-check-status.json && exit 0
+    jq -S . |
+    tr --delete '\n' ) \
+    <(tr --delete '\n' < .github/version-check-status.json)  > /dev/null && exit 0
+
+# Print the diff again leaving the newlines in
+diff -w <(rush check --json |
+    grep -A5000 -m1 -e '^$'|
+    tail -n +5 |
+    jq -S . ) \
+    .github/version-check-status.json  > /dev/null && exit 0
 
 echo Mismatches found. 
 echo Run \"rush check\" to confirm they were intended and update
