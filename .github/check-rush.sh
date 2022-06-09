1971517800d8b7fad3c59b9da18da0df30e33408
Silvio Tomatis <silviot@gmail.com>
2022-06-09 22:39:33 +0200

    Use -S option for jq to ensure stable ordering of keys

--- .github/check-rush.sh
+++ .github/check-rush.sh
@@ -6,10 +6,10 @@ echo Checking for newly inrtoduced dependency mismatches
 diff <(rush check --json |
     grep -A5000 -m1 -e '^$'|
     tail -n +5 |
-    jq) .github/version-check-status.json && exit 0
+    jq -S .) .github/version-check-status.json && exit 0
 
 echo Mismatches found. 
 echo Run \"rush check\" to confirm they were intended and update
 echo the versioned checked file with
-echo rush check --json \| grep -A5000 -m1 -e '^$'\|tail -n +5 \| jq  \> .github/version-check-status.json
+echo rush check --json \| grep -A5000 -m1 -e '^$'\|tail -n +5 \| jq -S . \> .github/version-check-status.json
 exit 1
