d2f80def31bc158cf916ef0645a48df58a604469
Silvio Tomatis <silviot@gmail.com>
2022-06-09 22:29:19 +0200

    Fix check rush exit code

--- .github/check-rush.sh
+++ .github/check-rush.sh
@@ -12,3 +12,4 @@ echo Mismatches found.
 echo Run \"rush check\" to confirm they were intended and update
 echo the versioned checked file with
 echo rush check --json \| grep -A5000 -m1 -e '^$'\|tail -n +5 \| jq  \> .github/version-check-status.json
+exit 1
