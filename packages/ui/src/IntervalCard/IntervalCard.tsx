0b6450e1be04c59471c875db5a7cab009be9fcba
ikusteu <ivan.kust57@gmail.com>
2022-06-10 17:16:49 +0200

    Add timestring in IntervalCard

--- packages/ui/src/IntervalCard/IntervalCard.tsx
+++ packages/ui/src/IntervalCard/IntervalCard.tsx
@@ -50,23 +50,29 @@ const IntervalCard: React.FC<IntervalCardProps> = ({
   const { t } = useTranslation();
   const date = DateTime.fromISO(dateISO);
 
+  const duration = calculateDuration(startTime, endTime);
+
   const dateString = (
-    <h1
+    <span
       className={[
         "text-base",
+        "block",
         variant === IntervalCardVariant.Simple
           ? "font-medium"
           : "font-semibold",
       ].join(" ")}
     >
       {t(DateFormat.Full, { date })}
-    </h1>
+    </span>
   );
 
-  const duration = calculateDuration(startTime, endTime);
+  const timestring = (
+    <span className={getTimestringClasses(variant, duration)}>
+      {[startTime, endTime].join(" - ")}
+    </span>
+  );
 
   const className = [
-    ...containerBaseClasses,
     ...getContainerSizeClasses(variant, duration),
     ...getBorderClasses(type, variant, state),
     getBackgroundColor(type, variant, state),
@@ -76,12 +82,34 @@ const IntervalCard: React.FC<IntervalCardProps> = ({
   return React.createElement(
     as,
     { className },
-    <>{variant !== IntervalCardVariant.Booking && dateString}</>
+    <>
+      {variant !== IntervalCardVariant.Booking && dateString}
+      {timestring}
+    </>
   );
 };
 // #endregion Component
 
-const containerBaseClasses = ["px-4", "py-3"];
+// #region timestringClasses
+const getTimestringClasses = (
+  variant: IntervalCardVariant,
+  duration: IntervalDuration
+) =>
+  variant !== IntervalCardVariant.Booking
+    ? timestringSizeLookup[variant].join(" ")
+    : timestringBookingSizeLookup[duration].join(" ");
+
+const timestringBookingSizeLookup = {
+  [IntervalDuration["1h"]]: ["text-lg", "leading-8", "font-semibold"],
+  [IntervalDuration["1.5h"]]: ["text-3xl", "leading-8", "font-semibold"],
+  [IntervalDuration["2h"]]: ["text-4xl", "leading-10", "font-semibold"],
+};
+
+const timestringSizeLookup = {
+  [IntervalCardVariant.Calendar]: ["text-3xl", "font-semibold"],
+  [IntervalCardVariant.Simple]: ["text-2xl", "font-medium"],
+};
+// #endregion timestringClasses
 
 // #region containerSize
 const getContainerSizeClasses = (
@@ -93,14 +121,44 @@ const getContainerSizeClasses = (
     : bookingContainerSizeLookup[duration];
 
 const containerSizeClassesLookup = {
-  [IntervalCardVariant.Calendar]: ["w-[280px]", "h-[152px]", "rounded-lg"],
-  [IntervalCardVariant.Simple]: ["w-[219px]", "h-20", "rounded-md"],
+  [IntervalCardVariant.Calendar]: [
+    "w-[280px]",
+    "h-[152px]",
+    "rounded-lg",
+    "px-4",
+    "py-3",
+  ],
+  [IntervalCardVariant.Simple]: [
+    "w-[219px]",
+    "h-20",
+    "rounded-md",
+    "px-4",
+    "py-3",
+  ],
 };
 
 const bookingContainerSizeLookup = {
-  [IntervalDuration["1h"]]: ["w-[220px]", "h-[110px]", "rounded-lg"],
-  [IntervalDuration["1.5h"]]: ["w-[320px]", "h-[128px]", "rounded-lg"],
-  [IntervalDuration["2h"]]: ["w-[401px]", "h-[146px]", "rounded-lg"],
+  [IntervalDuration["1h"]]: [
+    "w-[220px]",
+    "h-[110px]",
+    "rounded-lg",
+    "px-4",
+    "py-2.5",
+  ],
+  [IntervalDuration["1.5h"]]: [
+    "w-[320px]",
+    "h-[128px]",
+    "rounded-lg",
+    "px-4",
+    "py-3",
+  ],
+  [IntervalDuration["2h"]]: [
+    "w-[401px]",
+    "h-[146px]",
+    "rounded-lg",
+    "px-4",
+    "py-3",
+  ],
 };
 // #endregion containerSize
 
