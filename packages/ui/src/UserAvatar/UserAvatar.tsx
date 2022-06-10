d1bfb205a8add1042340fb110199fc6b6aad228e
delasous <sousa.yak@googlemail.com>
2022-06-10 11:55:59 +0200

    Add styles and fallback for undefined avatar

--- packages/ui/src/UserAvatar/UserAvatar.tsx
+++ packages/ui/src/UserAvatar/UserAvatar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { CustomerBase } from "@eisbuk//shared";
 
@@ -6,8 +6,66 @@ interface UserAvatarProps extends Pick<CustomerBase, "name" | "surname"> {
   avatar?: string;
 }
 
-const UserAvatar: React.FC<UserAvatarProps> = () => {
-  return <div></div>;
+/**
+ * Returns initials from provided name and last name
+ * @param name
+ * @param surname
+ * @returns
+ */
+const getInitials = (name: string, surname: string): string =>
+  `${name[0]}${surname[0]}`;
+
+const UserAvatar: React.FC<UserAvatarProps> = ({ name, surname, avatar }) => {
+  const [avatarUrl, setAvatarUrl] = useState(avatar);
+
+  const containerClassNames = containerClasses.join(" ");
+  const userNameClassNames = userNameClasses.join(" ");
+  const avatarClassNames = avatarClasses.join(" ");
+
+  return (
+    <div className={containerClassNames}>
+      <div>
+        <p className={userNameClassNames}>
+          {name} {surname}
+        </p>
+      </div>
+      <div className={avatarClassNames}>
+        {avatarUrl ? (
+          <img
+            className="rounded-full object-cover"
+            src={avatarUrl}
+            onError={({ currentTarget }) => {
+              currentTarget.onerror = null;
+              setAvatarUrl(undefined);
+            }}
+          />
+        ) : (
+          <p className="text-xl tracking-wider">{getInitials(name, surname)}</p>
+        )}
+      </div>
+    </div>
+  );
 };
+
+const containerClasses = [
+  "flex",
+  "min-w-min",
+  "items-center",
+  "space-x-4",
+  "text-white",
+  "select-none",
+];
+
+const userNameClasses = ["text-base", "leading-5", "font-medium"];
+
+const avatarClasses = [
+  "flex",
+  "items-center",
+  "justify-center",
+  "rounded-full",
+  "h-14",
+  "w-14",
+  "bg-teal-400",
+];
 
 export default UserAvatar;
