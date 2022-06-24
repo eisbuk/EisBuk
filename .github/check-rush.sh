c765d96fc5c7d1e2e0b2cbc9bd970321fcad3123
ikusteu <ivan.kust57@gmail.com>
2022-06-24 22:30:19 +0200

    Update check-rush command

--- .github/check-rush.sh
+++ .github/check-rush.sh
@@ -4,13 +4,12 @@ echo Checking for newly inrtoduced dependency mismatches
 
 # remove newlines
 diff -w <(rush check --json |
-    grep -A5000 -m1 -e '^$'|
-    tail -n +5 |
+    grep -A5000 -m1 -e '^{$'|
     jq -S . ) \
     <(jq -S . < .github/version-check-status.json) && exit 0
 
 echo Mismatches found. 
 echo Run \"rush check\" to confirm they were intended and update
 echo the versioned checked file with
-echo rush check --json \| grep -A5000 -m1 -e '^$'\|tail -n +5 \| jq -S . \> .github/version-check-status.json
+echo rush check --json \| grep -A5000 -m1 -e '^$'\|jq -S . \> .github/version-check-status.json
 exit 1
