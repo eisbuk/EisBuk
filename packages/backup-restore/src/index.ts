830c9ff22a6c8bfb2098783369bfe43f888fd4c4
delasous <sousa.yak@googlemail.com>
2022-06-24 16:25:05 +0200

    Export script functions from index (for use in e.g packages/functions cron-jobs

---
+++ packages/backup-restore/src/index.ts
@@ -0,0 +1,11 @@
+import {
+  backupAllOrganizations,
+  backupSingleOrganization,
+} from "./firestore/backupService";
+import { restoreOrganizations } from "./firestore/restoreService";
+
+export {
+  backupAllOrganizations,
+  backupSingleOrganization,
+  restoreOrganizations,
+};
