bf9dc61f6286fd2ad8b206b7f6232440e669381a
ikusteu <ivan.kust57@gmail.com>
2022-06-24 23:08:48 +0200

    Add minimal mobile styles and ability to receive 'className' in SlotsDayContainer

--- packages/ui/src/SlotsDayContainer/SlotsDayContainer.tsx
+++ packages/ui/src/SlotsDayContainer/SlotsDayContainer.tsx
@@ -5,21 +5,33 @@ import i18n, { DateFormat } from "@eisbuk/translations";
 
 interface SlotsDayContainerProps {
   date: DateTime;
+  className?: string;
   children?: React.ReactNode | React.ReactNode[];
 }
 
 const SlotsDayConatiner: React.FC<SlotsDayContainerProps> = ({
-  children,
   date,
+  className,
+  children,
 }) => {
   const dateString = i18n.t(DateFormat.Full, { date });
 
+  const containerClasses = [
+    "pt-[44px]",
+    "pb-8",
+    "border-b-2",
+    "border-gray-100",
+    "md:pt-[62px]",
+  ];
+
   return (
-    <div className="pb-8 pt-[62px] border-b-2 border-gray-100">
+    <div className={[...containerClasses, className].join(" ")}>
       <h1 className="text-2xl font-normal leading-none text-gray-700 mb-8">
         {dateString}
       </h1>
-      <div className="flex flex-wrap gap-6 items-end">{children}</div>
+      <div className="flex flex-wrap items-end gap-6 min-h-[146px]">
+        {children}
+      </div>
     </div>
   );
 };
