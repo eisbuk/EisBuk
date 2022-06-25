7e77d85a3ee8bc8802a7665061457b7728383daa
ikusteu <ivan.kust57@gmail.com>
2022-06-25 14:21:31 +0200

    Convert notification store tests to table tests

--- packages/client/src/store/actions/__tests__/notifications.test.ts
+++ packages/client/src/store/actions/__tests__/notifications.test.ts
@@ -7,14 +7,15 @@ import {
   nextNotification,
 } from "../notificationsActions";
 
-const testNotification1 = {
-  variant: NotifVariant.Success,
-  message: "We tha best music!!!",
-};
-const testNotification2 = {
-  variant: NotifVariant.Error,
-  message: "U can't touch this",
-};
+// #region tableTestRunner
+interface TestParams {
+  name: string;
+  testQueue: Omit<NotificationInterface, "key">[];
+  testCanEvict: boolean;
+  run: (store: ReturnType<typeof getNewStore>) => void;
+  wantQueue?: Omit<NotificationInterface, "key">[];
+  wantCanEvict?: boolean;
+}
 
 const setupQueue = (
   store: ReturnType<typeof getNewStore>,
@@ -25,134 +26,147 @@ const setupQueue = (
   }
 };
 
-describe("Notifications actions", () => {
-  describe("enqueueNotification", () => {
-    test("if queue empty, should enqueue notification assigning to it a random key and setting the 'canEvict' to false", () => {
-      const store = getNewStore();
+const runNotificationsTableTests = (tests: TestParams[]) => {
+  tests.forEach(
+    ({ name, testQueue, testCanEvict, run, wantQueue, wantCanEvict }) =>
+      test(name, () => {
+        // Fail for non existing scenario
+        if (testQueue.length > 1 && testCanEvict) {
+          fail(
+            `canEvict can only be 'true' on empty and single notification queue. The testQueue you've provided has length ${
+              testQueue.length
+            }. Test queue:
+                  ${JSON.stringify(testQueue)}        
+                  `
+          );
+        }
 
-      store.dispatch(enqueueNotification(testNotification1));
+        // Setup test
+        const store = getNewStore();
+        setupQueue(store, testQueue);
+        if (testCanEvict) {
+          // If 'canEvict' part of the setup, run 'nextNotification' setting it to 'true' in the store
+          // We've already handled the non existing cases of canEvict at the top of the function block
+          store.dispatch(nextNotification());
+        }
 
-      const { queue, canEvict } = store.getState().notifications;
-      const [{ key, ...enqueuedNotification }] = queue;
+        // Run the test
+        run(store);
 
-      expect(enqueuedNotification).toEqual(testNotification1);
-      expect(canEvict).toEqual(false);
-      expect(key).toBeTruthy();
-    });
+        // Make assertions
+        const { canEvict: resCanEvict, queue } = store.getState().notifications;
 
-    test("if notifications already enqueued, and 'canEvict = false' should add the new notification at the end of the queue", () => {
-      const store = getNewStore();
-      // By enqueueing notification(s) to set up the state, the canEvict is set to false
-      setupQueue(store, [testNotification1]);
+        if (wantQueue) {
+          // Get the resulting queue from store, but remove the randomly generated keys for easier comparisosn
+          const resQueue = queue.map(
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            ({ key, ...notification }) => notification
+          );
 
-      store.dispatch(enqueueNotification(testNotification2));
+          expect(resQueue).toEqual(wantQueue);
+        }
 
-      const { queue } = store.getState().notifications;
-      const [
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        { key: key1, ...firstNotification },
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        { key: key2, ...secondNotification },
-      ] = queue;
+        if (wantCanEvict) {
+          expect(resCanEvict).toEqual(wantCanEvict);
+        }
+      })
+  );
+};
+// #endregion tableTestRunner
 
-      expect(firstNotification).toEqual(testNotification1);
-      expect(secondNotification).toEqual(testNotification2);
-    });
+// #region tests
+const testNotification1 = {
+  variant: NotifVariant.Success,
+  message: "We tha best music!!!",
+};
+const testNotification2 = {
+  variant: NotifVariant.Error,
+  message: "U can't touch this",
+};
 
-    test("if notification already enqueued, but 'canEvict = true' should replace the enqueued notification with the new one", () => {
-      const store = getNewStore();
-      // By enqueueing notification(s) to set up the state, the 'canEvict' is set to false
-      setupQueue(store, [testNotification1]);
-      // By performing 'nextNotification' on single member queue, the 'canEvict' is set to true
-      store.dispatch(nextNotification());
+describe("Notification store table tests", () => {
+  runNotificationsTableTests([
+    {
+      name: "Enqueue action: if queue empty, should enqueue notification and set 'canEvict' to false",
+      testQueue: [],
+      testCanEvict: true,
+      run: (store) => {
+        store.dispatch(enqueueNotification(testNotification1));
+      },
+      wantQueue: [testNotification1],
+      wantCanEvict: false,
+    },
+    {
+      name: "Enqueue action: if queue not empty and 'canEvict' false, should enqueue new notificaiton at the end of the queue",
+      testQueue: [testNotification1],
+      testCanEvict: false,
+      run: (store) => {
+        store.dispatch(enqueueNotification(testNotification2));
+      },
+      wantQueue: [testNotification1, testNotification2],
+      wantCanEvict: false,
+    },
+    {
+      name: "Enqueue action: if queue not empty, but 'canEvict' true, should evict existing notification and replace with the new one",
+      testQueue: [testNotification1],
+      testCanEvict: true,
+      run: (store) => {
+        store.dispatch(enqueueNotification(testNotification2));
+      },
+      wantQueue: [testNotification2],
+      wantCanEvict: false,
+    },
 
-      store.dispatch(enqueueNotification(testNotification2));
+    {
+      name: "Next action: if queue longer than 1, should move to the next notification (by evicting the currently first)",
+      testQueue: [testNotification1, testNotification2],
+      testCanEvict: false,
+      run: (store) => {
+        store.dispatch(nextNotification());
+      },
+      wantQueue: [testNotification2],
+      wantCanEvict: false,
+    },
+    {
+      name: "Next action: if only one notification in queue, should not evict the notification, but set 'canEvict' to true",
+      testQueue: [testNotification1],
+      testCanEvict: false,
+      run: (store) => {
+        store.dispatch(nextNotification());
+      },
+      wantQueue: [testNotification1],
+      wantCanEvict: true,
+    },
 
-      const { queue, canEvict } = store.getState().notifications;
-      const [
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        { key, ...firstNotification },
-      ] = queue;
-
-      expect(queue.length).toEqual(1);
-      expect(firstNotification).toEqual(testNotification2);
-      expect(canEvict).toEqual(false);
-    });
-  });
-
-  describe("nextNotification", () => {
-    test("should remove the first notification from the queue if there are other notifications enqueued", () => {
-      const store = getNewStore();
-      setupQueue(store, [testNotification1, testNotification2]);
-
-      store.dispatch(nextNotification());
-
-      const { queue } = store.getState().notifications;
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const [{ key, ...firstNotification }] = queue;
-
-      expect(queue.length).toEqual(1);
-      expect(firstNotification).toEqual(testNotification2);
-    });
-
-    test("should not remove the notification from the queue if there's only the one, but should set canEvict to 'true'", () => {
-      const store = getNewStore();
-      setupQueue(store, [testNotification1]);
-
-      store.dispatch(nextNotification());
-
-      const { queue, canEvict } = store.getState().notifications;
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const [{ key, ...firstNotification }] = queue;
-
-      expect(queue.length).toEqual(1);
-      expect(firstNotification).toEqual(testNotification1);
-      expect(canEvict).toEqual(true);
-    });
-  });
-
-  describe("evictNotification", () => {
-    test("should remove the first notification regardless of additional notifications being enqueued or not, canEvict should be set to 'false'", () => {
-      const store = getNewStore();
-      setupQueue(store, [testNotification1, testNotification2]);
-
-      // Evict once, removing the first notification while the second enqueued becomes the first
-      store.dispatch(evictNotification());
-      const { queue: queue1, canEvict: canEvict1 } =
-        store.getState().notifications;
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const [{ key, ...firstNotification1 }] = queue1;
-      expect(queue1.length).toEqual(1);
-      expect(firstNotification1).toEqual(testNotification2);
-      expect(canEvict1).toEqual(false);
-
-      // Evict again to assure the norification will get removed, regardless of it being the only one in the queue
-      // resulting in an empty queue
-      store.dispatch(evictNotification());
-      const { queue: queue2, canEvict: canEvict2 } =
-        store.getState().notifications;
-      expect(queue2).toEqual([]);
-      expect(canEvict2).toEqual(false);
-    });
-
-    test("should set 'canEvict' to false when evicting", () => {
-      const store = getNewStore();
-      // canEvict is set to 'false' when setting up the state
-      setupQueue(store, [testNotification1]);
-      // Set canEvict to 'true' by performing 'nextNotification' on one notification queue
-      store.dispatch(nextNotification());
-
-      // Evict once, removing the first notification while the second enqueued becomes the first
-      store.dispatch(evictNotification());
-      const { canEvict: canEvict } = store.getState().notifications;
-      expect(canEvict).toEqual(false);
-    });
-
-    test("should not break if performed on an empty queue", () => {
-      const store = getNewStore();
-      store.dispatch(evictNotification());
-      const { queue } = store.getState().notifications;
-      expect(queue).toEqual([]);
-    });
-  });
+    {
+      name: "Evict action: should remove first notification regardless of it being the only one",
+      testQueue: [testNotification1],
+      testCanEvict: false,
+      run: (store) => {
+        store.dispatch(evictNotification());
+      },
+      wantQueue: [],
+      wantCanEvict: false,
+    },
+    {
+      name: "Evict action: should always set 'canEvict' to false when evicting",
+      testQueue: [testNotification1],
+      testCanEvict: true,
+      run: (store) => {
+        store.dispatch(evictNotification());
+      },
+      wantQueue: [],
+      wantCanEvict: false,
+    },
+    {
+      name: "Evict action: should not break on empty queue",
+      testQueue: [],
+      testCanEvict: true,
+      run: (store) => {
+        store.dispatch(evictNotification());
+      },
+      wantQueue: [],
+    },
+  ]);
 });
+// #endregion tests
