97a9081b5a2855650b5b441077378dea9b879557
ikusteu <ivan.kust57@gmail.com>
2022-06-25 12:32:20 +0200

    Develop and test the functionality of 'nextNotification' action

--- packages/client/src/store/actions/__tests__/notifications.test.ts
+++ packages/client/src/store/actions/__tests__/notifications.test.ts
@@ -1,32 +1,46 @@
 import { NotifVariant } from "@/enums/store";
 import { getNewStore } from "@/store/createStore";
-import { enqueueNotification } from "../notificationsActions";
+import { NotificationInterface } from "@/store/reducers/notificationsReducer";
+import { enqueueNotification, nextNotification } from "../notificationsActions";
 
-const testNotification = {
+const testNotification1 = {
   variant: NotifVariant.Success,
   message: "We tha best music!!!",
+};
+const testNotification2 = {
+  variant: NotifVariant.Error,
+  message: "U can't touch this",
+};
+
+const setupQueue = (
+  store: ReturnType<typeof getNewStore>,
+  notifications: Array<Omit<NotificationInterface, "key">>
+) => {
+  for (const notification of notifications) {
+    store.dispatch(enqueueNotification(notification));
+  }
 };
 
 describe("Notifications actions", () => {
   describe("enqueueNotification", () => {
     test("should enqueue notification assigning to it a random key", () => {
       const store = getNewStore();
-      store.dispatch(enqueueNotification(testNotification));
+
+      store.dispatch(enqueueNotification(testNotification1));
+
       const { queue } = store.getState().notifications;
       const [{ key, ...enqueuedNotification }] = queue;
 
-      expect(enqueuedNotification).toEqual(testNotification);
+      expect(enqueuedNotification).toEqual(testNotification1);
       expect(key).toBeTruthy();
     });
 
     test("if notifications already enqueued, should add the new notification at the end of the queue", () => {
-      const existingNotification = {
-        variant: NotifVariant.Error,
-        message: "U can't touch this",
-      };
       const store = getNewStore();
-      store.dispatch(enqueueNotification(existingNotification));
-      store.dispatch(enqueueNotification(testNotification));
+      setupQueue(store, [testNotification1]);
+
+      store.dispatch(enqueueNotification(testNotification2));
+
       const { queue } = store.getState().notifications;
       const [
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -34,8 +48,39 @@ describe("Notifications actions", () => {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         { key: key2, ...secondNotification },
       ] = queue;
-      expect(firstNotificaiton).toEqual(existingNotification);
-      expect(secondNotification).toEqual(testNotification);
+
+      expect(firstNotificaiton).toEqual(testNotification1);
+      expect(secondNotification).toEqual(testNotification2);
+    });
+  });
+
+  describe("nextNotification", () => {
+    test("should remove the first notification from the queue if there are other notifications enqueued", () => {
+      const store = getNewStore();
+      setupQueue(store, [testNotification1, testNotification2]);
+
+      store.dispatch(nextNotification());
+
+      const { queue } = store.getState().notifications;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [{ key, ...firstNotificaiton }] = queue;
+
+      expect(queue.length).toEqual(1);
+      expect(firstNotificaiton).toEqual(testNotification2);
+    });
+
+    test("should do nothing if there's only one notification in the queue", () => {
+      const store = getNewStore();
+      setupQueue(store, [testNotification1]);
+
+      store.dispatch(nextNotification());
+
+      const { queue } = store.getState().notifications;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [{ key, ...firstNotificaiton }] = queue;
+
+      expect(queue.length).toEqual(1);
+      expect(firstNotificaiton).toEqual(testNotification1);
     });
   });
 });
