74339f90e7476eebcbe9bb28206fb867efe93023
ikusteu <ivan.kust57@gmail.com>
2022-06-26 17:02:35 +0200

    Add countdown and i18n message to BookingsCountdown

--- packages/ui/src/BookingsCountdown/BookingsCountdown.stories.tsx
+++ packages/ui/src/BookingsCountdown/BookingsCountdown.stories.tsx
@@ -12,7 +12,8 @@ export default {
 } as ComponentMeta<typeof BookingsCountdown>;
 
 // Show countdown until two days from now (to keep elements dislpayed constant for visual regression)
-const countdownDate = DateTime.now().plus({ days: 2 });
+const deadline = DateTime.now().plus({ days: 2 });
+const month = deadline.plus({ months: 1 });
 
 const message = "Bookings end in:";
 
@@ -20,20 +21,20 @@ export const Variants = (): JSX.Element => (
   <>
     <h1 className="text-lg font-bold mb-4">Variant: "first-deadline"</h1>
     <BookingsCountdown
-      className="mb-8 w-sm"
-      {...{ countdownDate, message }}
+      className="mb-8 md:w-sm w-full"
+      {...{ deadline, message, month }}
       variant={BookingsCountdownVariant.FirstDeadline}
     />
     <h1 className="text-lg font-bold mb-4">Variant: "second-deadline"</h1>
     <BookingsCountdown
-      className="mb-8 w-sm"
-      {...{ countdownDate, message }}
+      className="mb-8 md:w-sm w-full"
+      {...{ deadline, message, month }}
       variant={BookingsCountdownVariant.SecondDeadline}
     />
     <h1 className="text-lg font-bold mb-4">Variant: "bookings-locked"</h1>
     <BookingsCountdown
-      className="mb-8 w-sm"
-      {...{ countdownDate, message }}
+      className="mb-8 md:w-sm w-full"
+      {...{ deadline, message, month }}
       variant={BookingsCountdownVariant.BookingsLocked}
     />
   </>
