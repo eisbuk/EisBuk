aeeb0ef825574fce1e94a86478cc4518842e7820
ikusteu <ivan.kust57@gmail.com>
2022-06-26 09:45:03 +0200

    Add children to the Layout component

--- packages/ui/src/Layout/Layout.stories.tsx
+++ packages/ui/src/Layout/Layout.stories.tsx
@@ -37,6 +37,20 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 export const Default = (): JSX.Element => <Layout {...{ user }} />;
+
 export const CustomerArea = (): JSX.Element => (
-  <Layout {...{ additionalButtons, Notifications, user }} />
+  <Layout {...{ additionalButtons, Notifications, user }}>
+    <div className="content-container">
+      <div className="md:px-[44px]">
+        {Array(8)
+          .fill(null)
+          .map((_, i) => (
+            <div
+              key={i}
+              className="w-full h-40 my-4 bg-gray-100 border-2 border-gray-200 rounded-md md:h-80"
+            />
+          ))}
+      </div>
+    </div>
+  </Layout>
 );
--- packages/ui/src/Layout/Layout.tsx
+++ packages/ui/src/Layout/Layout.tsx
@@ -9,6 +9,7 @@ interface LayoutProps {
   user?: UserAvatarProps;
   additionalButtons?: JSX.Element;
   Notifications?: React.FC<{ className?: string }>;
+  children?: React.ReactNode[] | React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({
@@ -16,29 +17,34 @@ const Layout: React.FC<LayoutProps> = ({
   additionalButtons,
   user,
   Notifications,
+  children,
 }) => {
   const rowClasses = "flex justify-between items-center w-full min-h-[70px]";
 
   return (
-    <div className="bg-gray-800">
-      <div className="content-container">
-        <div className={rowClasses}>
-          <div className="h-5 w-[86px] text-white">{<Logo />}</div>
+    <div>
+      <header className="bg-gray-800">
+        <div className="content-container">
+          <div className={rowClasses}>
+            <div className="h-5 w-[86px] text-white">{<Logo />}</div>
 
-          {user && <UserAvatar {...user} />}
-        </div>
-
-        <div className="w-full h-[2px] bg-gray-700" />
-
-        <div className={[rowClasses, "py-[15px]"].join(" ")}>
-          <div className="w-full flex justify-center gap-4 md:gap-3 md:justify-start md:max-w-1/2">
-            {additionalButtons}
+            {user && <UserAvatar {...user} />}
           </div>
-          <div className="fixed bottom-[27px] left-4 right-4 md:static">
-            {Notifications && <Notifications className="w-full md:w-auto" />}
+
+          <div className="w-full h-[2px] bg-gray-700" />
+
+          <div className={[rowClasses, "py-[15px]"].join(" ")}>
+            <div className="w-full flex justify-center gap-4 md:gap-3 md:justify-start md:max-w-1/2">
+              {additionalButtons}
+            </div>
+            <div className="fixed bottom-[27px] left-4 right-4 md:static">
+              {Notifications && <Notifications className="w-full md:w-auto" />}
+            </div>
           </div>
         </div>
-      </div>
+      </header>
+
+      <main>{children}</main>
     </div>
   );
 };
