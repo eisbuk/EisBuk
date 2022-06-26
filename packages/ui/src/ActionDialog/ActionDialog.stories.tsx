84b3d39645fc10100da418d006770ed97b0f0292
ikusteu <ivan.kust57@gmail.com>
2022-06-26 21:53:46 +0200

    Stretch the ActionDialog buttons on small screen

--- packages/ui/src/ActionDialog/ActionDialog.stories.tsx
+++ packages/ui/src/ActionDialog/ActionDialog.stories.tsx
@@ -9,18 +9,17 @@ export default {
 } as ComponentMeta<typeof ActionDialog>;
 
 export const Default = (): JSX.Element => (
-  <div className="w-screen h-screen bg-gray-800">
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-      <ActionDialog
-        title="Cancel booking for"
-        cancelLabel="Go Back"
-        confirmLabel="Confirm Cancellation"
-        onCancel={() => {}}
-        onConfirm={() => {}}
-      >
-        This is a placeholder message for a Simple Interval Card, but also stunt
-        doubles as a TextContent variant?
-      </ActionDialog>
-    </div>
+  <div className="absolute top-0 right-0 bottom-0 left-0 bg-gray-800">
+    <ActionDialog
+      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
+      title="Cancel booking for"
+      cancelLabel="Go Back"
+      confirmLabel="Confirm Cancellation"
+      onCancel={() => {}}
+      onConfirm={() => {}}
+    >
+      This is a placeholder message for a Simple Interval Card, but also stunt
+      doubles as a TextContent variant?
+    </ActionDialog>
   </div>
 );
--- packages/ui/src/ActionDialog/ActionDialog.tsx
+++ packages/ui/src/ActionDialog/ActionDialog.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Button from "../Button";
 import { ButtonSize } from "../Button/Button";
 
-export interface ActionDialogProps {
+export interface ActionDialogProps extends React.HTMLAttributes<HTMLElement> {
   title: string;
   onCancel: () => void;
   onConfirm: () => void;
@@ -18,24 +18,26 @@ const ActionDialog: React.FC<ActionDialogProps> = ({
   confirmLabel = "Confirm",
   cancelLabel = "Cancel",
   children,
+  className = "",
 }) => (
-  <div className="flex flex-col max-w-sm items-start gap-y-4 p-5 rounded-md bg-white">
-    <div>
-      <h3 className="text-base leading-6 font-semibold text-red-700">
-        {title}
-      </h3>
-    </div>
+  <div
+    className={[
+      "flex flex-col min-w-[320px] items-start gap-y-4 p-5 rounded-md bg-white",
+      className,
+    ].join(" ")}
+  >
+    <h3 className="text-base leading-6 font-semibold text-red-700">{title}</h3>
     <div>{children}</div>
-    <div className="flex space-x-2">
+    <div className="w-full flex gap-2 flex-wrap justify-end">
       <Button
-        className="!text-gray-700 font-medium bg-gray-100 hover:bg-opacity-0"
+        className="w-full !text-gray-700 font-medium bg-gray-100 hover:bg-opacity-0 md:w-auto"
         size={ButtonSize.LG}
         onClick={onCancel}
       >
         {cancelLabel}
       </Button>
       <Button
-        className="!text-red-700 bg-red-200 hover:bg-red-100"
+        className="w-full !text-red-700 bg-red-200 hover:bg-red-100 md:w-auto"
         size={ButtonSize.LG}
         onClick={onConfirm}
       >
