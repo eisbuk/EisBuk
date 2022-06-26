22940c5ac1535e4aff73e48a30ba60360e9e680f
ikusteu <ivan.kust57@gmail.com>
2022-06-26 10:16:37 +0200

    Hide header bottom row on mobile if no additional buttons are shown

--- packages/ui/src/Layout/Layout.tsx
+++ packages/ui/src/Layout/Layout.tsx
@@ -19,13 +19,11 @@ const Layout: React.FC<LayoutProps> = ({
   Notifications,
   children,
 }) => {
-  const rowClasses = "flex justify-between items-center w-full min-h-[70px]";
-
   return (
     <div>
       <header className="bg-gray-800">
         <div className="content-container">
-          <div className={rowClasses}>
+          <div className={getHeaderRowClasses("top")}>
             <div className="h-5 w-[86px] text-white">{<Logo />}</div>
 
             {user && <UserAvatar {...user} />}
@@ -33,8 +31,13 @@ const Layout: React.FC<LayoutProps> = ({
 
           <div className="w-full h-[2px] bg-gray-700" />
 
-          <div className={[rowClasses, "py-[15px]"].join(" ")}>
-            <div className="w-full flex justify-center gap-4 md:gap-3 md:justify-start md:max-w-1/2">
+          <div
+            className={getHeaderRowClasses(
+              "bottom",
+              Boolean(additionalButtons)
+            )}
+          >
+            <div className="w-full flex justify-center items-center gap-4 md:gap-3 md:justify-start md:max-w-1/2">
               {additionalButtons}
             </div>
             <div className="fixed bottom-[27px] left-4 right-4 md:static">
@@ -47,6 +50,29 @@ const Layout: React.FC<LayoutProps> = ({
       <main>{children}</main>
     </div>
   );
+};
+
+/** Get styles for top / botton row of the header */
+const getHeaderRowClasses = (
+  row: "top" | "bottom",
+  hasAdditionalButtons?: boolean
+) => {
+  // Hide bottom row on mobile if it doesn't have 'additionalButtons'
+  const displayClasses =
+    row === "top" || hasAdditionalButtons ? ["flex"] : ["hidden", "md:flex"];
+
+  // Styles used across both rows
+  const baseClasses = [
+    "w-full",
+    "min-h-[70px]",
+    "justify-between",
+    "items-center",
+  ];
+
+  // Additional classes per row
+  const additionalClasses = row === "top" ? [] : ["py-[15px]"];
+
+  return [...displayClasses, ...baseClasses, ...additionalClasses].join(" ");
 };
 
 export default Layout;
