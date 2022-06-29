e1f1d1a7dd13a69df60e82df14c9e34c7805724b
ikusteu <ivan.kust57@gmail.com>
2022-06-29 19:57:10 +0200

    "Fix" the tests failing

--- packages/client/src/features/modal/index.ts
+++
@@ -1,8 +0,0 @@
-import Modal from "./Modal";
-import reducer from "./reducer";
-
-export * from "./actions";
-
-export { reducer };
-
-export default Modal;
--- packages/client/src/store/reducers/rootReducer.ts
+++ packages/client/src/store/reducers/rootReducer.ts
@@ -4,7 +4,7 @@ import appReducer from "./appReducer";
 import authReducer from "./authReducer";
 import copyPasteReducer from "./copyPasteReducer";
 import firestoreReducer from "@/react-redux-firebase/reducer";
-import { reducer as modalReducer } from "@/features/modal";
+import modalReducer from "@/features/modal/reducer";
 
 const rootReducer = combineReducers({
   firestore: firestoreReducer,
