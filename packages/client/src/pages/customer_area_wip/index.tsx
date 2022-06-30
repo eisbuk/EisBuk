e52c06438181d4a165d9bcc81352a127307d31b2
ikusteu <ivan.kust57@gmail.com>
2022-06-30 14:24:59 +0200

    Assemble the components into static rehauled attendance page

--- packages/client/src/pages/customer_area_wip/index.tsx
+++ packages/client/src/pages/customer_area_wip/index.tsx
@@ -1,8 +1,56 @@
 import React from "react";
-import { Layout } from "@eisbuk/ui";
+import { DateTime } from "luxon";
+
+import {
+  CalendarNav,
+  IntervalCardGroup,
+  Layout,
+  SlotsDayContainer,
+  TabItem,
+} from "@eisbuk/ui";
+import { Calendar, AccountCircle } from "@eisbuk/svg";
+
+// #region temporaryTestData
+import { SlotType } from "@eisbuk/shared";
+
+import { saul } from "@/__testData__/customers";
+import { baseSlot } from "@/__testData__/slots";
+
+const testDate = DateTime.fromISO("2022-01-01");
+
+const daysToRender = Array(7)
+  .fill(null)
+  .map((_, i) => testDate.plus({ days: i }));
+
+const slotsToRender = [baseSlot, { ...baseSlot, type: SlotType.OffIce }];
+// #region temporaryTestData
 
 const CustomerArea: React.FC = () => {
-  return <Layout></Layout>;
+  const additionalButtons = (
+    <>
+      <TabItem Icon={Calendar as any} label="Book" />
+      <TabItem Icon={AccountCircle as any} label="Calendar" />
+    </>
+  );
+
+  return (
+    <Layout additionalButtons={additionalButtons} user={saul}>
+      <div className="w-full">
+        <CalendarNav date={testDate} jump="month" />
+        <div className="content-container">
+          <div className="px-[44px]">
+            {daysToRender.map((day) => (
+              <SlotsDayContainer key={day.toISODate()} date={day}>
+                {slotsToRender.map((slot) => (
+                  <IntervalCardGroup key={slot.type} {...slot} />
+                ))}
+              </SlotsDayContainer>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default CustomerArea;
