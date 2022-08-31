e0c5ac1e90fcf6d96e8e3905090695cabacd62a8
ikusteu <ivan.kust57@gmail.com>
2022-08-31 17:31:49 +0200

    Update comparePeriods function to return 0 when periods are equal, update tests accordingly

--- packages/client/src/utils/__tests__/utils.test.ts
+++ packages/client/src/utils/__tests__/utils.test.ts
@@ -89,23 +89,46 @@ describe("Date utils", () => {
     });
   });
 
-  describe("`comparePeriods` function", () => {
-    test("should return earlier periods first", () => {
-      let original = ["13:30-14:00", "13:00-13:30"];
-      let expected = ["13:00-13:30", "13:30-14:00"];
-      expect(original.sort(comparePeriods)).toEqual(expected);
-      original = ["13:00-13:30", "12:30-13:00"];
-      expected = ["12:30-13:00", "13:00-13:30"];
-      expect(original.sort(comparePeriods)).toEqual(expected);
-    });
+  describe("`comparePeriods` function", () =>
+    comparePeriodsTableTests([
+      {
+        name: "should return earlier periods first",
+        input: ["13:30-14:00", "12:30-13:00", "13:00-13:30"],
+        want: ["12:30-13:00", "13:00-13:30", "13:30-14:00"],
+      },
+      {
+        name: "should return longer periods first",
+        input: ["13:00-14:00", "13:00-14:30", "13:00-13:30"],
+        want: ["13:00-14:30", "13:00-14:00", "13:00-13:30"],
+      },
+      {
+        name: "start time should take presedence over interval length",
+        input: ["13:30-15:00", "12:30-13:30", "12:00-12:30"],
+        want: ["12:00-12:30", "12:30-13:30", "13:30-15:00"],
+      },
+      {
+        name: "should not explode when sorting two equal periods",
+        input: ["13:00-15:00", "12:00-14:00", "13:00-15:00"],
+        want: ["12:00-14:00", "13:00-15:00", "13:00-15:00"],
+      },
+    ]));
 
-    test("should return longer periods (starting at the same time) first", () => {
-      let original = ["13:00-14:00", "13:00-14:30"];
-      let expected = ["13:00-14:30", "13:00-14:00"];
-      expect(original.sort(comparePeriods)).toEqual(expected);
-      original = ["13:00-13:30", "13:00-14:00"];
-      expected = ["13:00-14:00", "13:00-13:30"];
-      expect(original.sort(comparePeriods)).toEqual(expected);
-    });
+  test("Edge case: should return 0 if periods are the same (this will be used for composition of compare functions)", () => {
+    expect(comparePeriods("12:00", "12:00")).toEqual(0);
   });
 });
+
+interface ComparePeriodsTest {
+  name: string;
+  input: string[];
+  want: string[];
+}
+
+/** Table test func */
+function comparePeriodsTableTests(tests: ComparePeriodsTest[]) {
+  tests.forEach(({ name, input, want }) => {
+    test(name, () => {
+      expect(input.sort(comparePeriods)).toEqual(want);
+    });
+  });
+}
