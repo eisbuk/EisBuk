e6f4efaa828d4714ba32641ceaedb6f388b95b3d
ikusteu <ivan.kust57@gmail.com>
2022-09-04 23:21:04 +0200

    Update countryCode selector not to explode when theres no country code set up

--- packages/client/src/store/selectors/orgInfo.ts
+++ packages/client/src/store/selectors/orgInfo.ts
@@ -1,4 +1,5 @@
 import { LocalStore } from "@/types/store";
 
 export const getCountryCode = (state: LocalStore) =>
-  Object.values(state.firestore.data.publicOrgInfo || {})[0].countryCode || "";
+  (Object.values(state.firestore.data.publicOrgInfo || {})[0] || {})
+    .countryCode || "";
