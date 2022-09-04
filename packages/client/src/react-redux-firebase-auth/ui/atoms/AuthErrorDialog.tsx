379d04bc04f2e0132d3768877e27ecc2e2db417e
ikusteu <ivan.kust57@gmail.com>
2022-09-04 16:19:19 +0200

    Apply Tailwind styles to AuthErrorDialog

--- packages/client/src/react-redux-firebase-auth/ui/atoms/AuthErrorDialog.tsx
+++ packages/client/src/react-redux-firebase-auth/ui/atoms/AuthErrorDialog.tsx
@@ -1,9 +1,5 @@
 import React, { useRef } from "react";
 
-import Typography from "@mui/material/Typography";
-
-import makeStyles from "@mui/styles/makeStyles";
-
 import { useTranslation, ActionButton } from "@eisbuk/translations";
 
 import useClickOutside from "@/hooks/useClickOutside";
@@ -19,7 +15,6 @@ const AuthErrorDialog: React.FC<Props> = ({
   open = true,
   onClose = () => {},
 }) => {
-  const classes = useStyles();
   const { t } = useTranslation();
 
   const containerRef = useRef<HTMLDivElement>(null);
@@ -30,38 +25,21 @@ const AuthErrorDialog: React.FC<Props> = ({
   if (!open) return null;
 
   return (
-    <div ref={containerRef} className={classes.container}>
-      <Typography className={classes.errorMessage} variant="caption">
+    <div
+      ref={containerRef}
+      className="absolute px-4 py-2 top-0 left-[10%] right-[10%] z-50 text-center bg-yellow-100 border border-yellow-500"
+    >
+      <span className="text-xs leading-6 tracking-wide align-center text-black">
         <span>{message} </span>
-        <span className={classes.dismiss} onClick={onClose}>
+        <span
+          className="text-blue-600 font-normal cursor-pointer hover:underline"
+          onClick={onClose}
+        >
           {t(ActionButton.Dismiss)}
         </span>
-      </Typography>
+      </span>
     </div>
   );
 };
-
-const useStyles = makeStyles(() => ({
-  container: {
-    position: "absolute",
-    zIndex: 1000,
-    top: 0,
-    left: "10%",
-    right: "10%",
-    padding: "0.5rem 1rem",
-    textAlign: "center",
-    backgroundColor: "#f9edbe",
-    border: "1px solid #f0c36d",
-  },
-  errorMessage: {
-    fontSize: 12,
-    font: "Roboto,Arial, sans-serif",
-    color: "#000000",
-  },
-  dismiss: {
-    color: "#4285f4",
-    cursor: "pointer",
-  },
-}));
 
 export default AuthErrorDialog;
