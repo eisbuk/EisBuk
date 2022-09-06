fd3626cad504dfbdf7fbe2bd1c8e219288321314
ikusteu <ivan.kust57@gmail.com>
2022-09-06 19:02:47 +0200

    Develop and test prefixing functionality input change in 'PhoneInput'

--- packages/ui/src/PhoneInput/PhoneInput.tsx
+++ packages/ui/src/PhoneInput/PhoneInput.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { FieldInputProps } from "formik";
+import React, { useState } from "react";
 
 import TextInput, { DropdownAdornment, TextInputProps } from "../TextInput";
 
@@ -6,13 +7,47 @@ interface Props extends Omit<TextInputProps, "type" | "inputMode"> {
   defaultCountry?: string;
 }
 
-const PhoneInput: React.FC<Props> = (props) => (
-  <TextInput
-    {...props}
-    StartAdornment={
-      <DropdownAdornment label="country" options={["US", "CA", "EU"]} />
-    }
-  />
-);
+const dummyCountryCodes = [
+  {
+    label: "IT",
+    value: "+39",
+  },
+  {
+    label: "FR",
+    value: "+33",
+  },
+  {
+    label: "HR",
+    value: "+385",
+  },
+];
+
+const PhoneInput: React.FC<Props> = ({ field: f, ...props }) => {
+  const [prefix, setPrefix] = useState("");
+
+  const { onChange, value: v, name, ...field } = f as FieldInputProps<string>;
+  const value = v.replace(prefix, "");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Call 'onChange' with the change event as is
+    // but create final value by prepending input with country prefix
+    const value = prefix + e.target.value;
+    onChange({ target: { name, value } });
+  };
+
+  return (
+    <TextInput
+      {...props}
+      field={{ ...field, value, name, onChange: handleChange }}
+      StartAdornment={
+        <DropdownAdornment
+          onChange={(e) => setPrefix(e.target.value)}
+          label="country"
+          options={dummyCountryCodes}
+        />
+      }
+    />
+  );
+};
 
 export default PhoneInput;
