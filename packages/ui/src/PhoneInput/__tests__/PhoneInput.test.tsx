6c9b4fc1f6f16af45431e02a88591d6ec0209977
ikusteu <ivan.kust57@gmail.com>
2022-09-06 19:02:47 +0200

    Develop and test prefixing functionality input change in 'PhoneInput'

--- packages/ui/src/PhoneInput/__tests__/PhoneInput.test.tsx
+++ packages/ui/src/PhoneInput/__tests__/PhoneInput.test.tsx
@@ -1,15 +1,39 @@
-import { render } from "@testing-library/react";
-import { Field, Formik } from "formik";
 import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Field, Form, Formik, FormikConfig } from "formik";
 
 import PhoneInput from "../PhoneInput";
 
 describe("PhoneInput", () => {
-  test("somke test", () => {
+  test("should prepend phone input with the selected country dial code", async () => {
+    // We only care about the submit valules for this test case
+    const mockSubmit = jest.fn();
+    const handleSubmit: FormikConfig<{ phone: string }>["onSubmit"] = (
+      values
+    ) => mockSubmit(values);
+
     render(
-      <Formik initialValues={{}} onSubmit={() => {}}>
-        <Field name="phone" component={PhoneInput} />
+      <Formik initialValues={{ phone: "" }} onSubmit={handleSubmit}>
+        <Form>
+          <Field name="phone" component={PhoneInput} />
+          <button type="submit">Submit</button>
+        </Form>
       </Formik>
     );
+
+    // Select 'HR', Croatia, dial code: "+385"
+    userEvent.selectOptions(
+      screen.getByRole("combobox"),
+      screen.getByRole("option", { name: "HR" })
+    );
+
+    // Type in a generic phone number (we're expecting this to be prepended with "+385")
+    userEvent.type(screen.getByRole("textbox"), "991234567");
+
+    screen.getByRole("button").click();
+    await waitFor(() => {
+      expect(mockSubmit).toHaveBeenCalledWith({ phone: "+385991234567" });
+    });
   });
 });
