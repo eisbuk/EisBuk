b902dd5968ea17297a574d5d000d46485a4f7356
ikusteu <ivan.kust57@gmail.com>
2022-09-07 12:09:43 +0200

    Make sure the 'defaultCountry' is immedietely passed to the dropdown as a value (make it a controlled component)

--- packages/ui/src/PhoneInput/PhoneInput.tsx
+++ packages/ui/src/PhoneInput/PhoneInput.tsx
@@ -79,6 +79,7 @@ const PhoneInput: React.FC<Props> = ({
           label="country"
           options={countryCodeOptions}
           className="border-0 border-r !rounded-r-none !border-gray-300 px-6 cursor-pointer"
+          value={countryCode}
         />
       }
     />
