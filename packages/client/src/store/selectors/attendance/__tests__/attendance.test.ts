f4efcec40d020945773698a8dd47ae4888279fba
delasous <sousa.yak@googlemail.com>
2022-09-20 09:31:33 +0200

    Add attendance variance selector logic: format attendance records as table data entries

--- packages/client/src/store/selectors/attendance/__tests__/attendance.test.ts
+++ packages/client/src/store/selectors/attendance/__tests__/attendance.test.ts
@@ -7,8 +7,10 @@ import { changeCalendarDate } from "@/store/actions/appActions";
 
 import { getSlotsWithAttendance } from "../slotAttendance";
 import {
+  collectAttendanceByCustomer,
   filterAttendanceByMonth,
   flattenAndConvertAttendanceIntervals,
+  formatToTableData,
 } from "../attendanceVariance";
 
 import { AttendanceSortBy } from "@/enums/other";
@@ -20,7 +22,8 @@ import {
   expectedStructByName,
   expectedStructByBookedInterval,
 } from "../../__testData__/attendance";
-import { testDateLuxon } from "@/__testData__/date";
+import { testDate, testDateLuxon } from "@/__testData__/date";
+import { walt, jian, saul } from "@/__testData__/customers";
 
 describe("Selectors ->", () => {
   describe("Test 'getSlotsWithAttendance'", () => {
@@ -81,6 +84,52 @@ describe("Selectors ->", () => {
       expect(result).toHaveProperty("customerId");
       expect(typeof result.attendedInterval).toBe("number");
       expect(typeof result.bookedInterval).toBe("number");
+    });
+
+    test("should collect attendance by customer", () => {
+      // * Working only with "slot-0", and "slot-1" of /__testsData__/attendance => line 70 tests attendance records
+      const [attendanceDoc1, attendanceDoc2] = attendanceArr;
+
+      const flatAttendanceRecords = [attendanceDoc1, attendanceDoc2].reduce(
+        flattenAndConvertAttendanceIntervals,
+        []
+      );
+
+      const result = flatAttendanceRecords.reduce(
+        collectAttendanceByCustomer,
+        {}
+      );
+
+      expect(result).toHaveProperty(walt.id);
+      expect(result).toHaveProperty(jian.id);
+      expect(result).toHaveProperty(saul.id);
+
+      const jiansAttendanceRecords = result[jian.id];
+      expect(jiansAttendanceRecords.length).toBe(2);
+    });
+
+    test("should format CustomerAttendanceRecords to TableData entries", () => {
+      // * Working only with "slot-0", and "slot-1" of /__testsData__/attendance => line 70 tests attendance records
+      const [attendanceDoc1, attendanceDoc2] = attendanceArr;
+
+      const customerAttendance = [attendanceDoc1, attendanceDoc2]
+        .reduce(flattenAndConvertAttendanceIntervals, [])
+        .reduce(collectAttendanceByCustomer, {});
+
+      const result = Object.entries(customerAttendance).map(
+        formatToTableData(attendanceCustomers)
+      );
+
+      const [firstEntry, secondEntry, thirdEntry] = result;
+
+      expect(result.length).toBe(3);
+      expect(firstEntry).toHaveProperty("athlete");
+      expect(firstEntry.hours).toHaveProperty(testDate);
+
+      // testDate attendance hours should now be collected as tuple: [booked, attended]
+      expect(firstEntry.hours[testDate]).toHaveLength(2);
+      expect(secondEntry.hours[testDate]).toHaveLength(2);
+      expect(thirdEntry.hours[testDate]).toHaveLength(2);
     });
   });
 });
--- packages/client/src/store/selectors/attendance/attendanceVariance.ts
+++ packages/client/src/store/selectors/attendance/attendanceVariance.ts
@@ -1,38 +1,43 @@
-import { SlotAttendnace } from "@eisbuk/shared";
+import { SlotAttendnace, Customer } from "@eisbuk/shared";
 
 import { LocalStore } from "@/types/store";
 import { getMonthStr, convertIntervalToNum } from "@/utils/helpers";
+import { type DayAttendanceHours } from "@eisbuk/ui";
 
-interface AttendanceRecord {
-  date: string;
+interface AttendanceRecord extends CustomerAttendanceRecord {
   customerId: string;
-  attendedInterval: number;
-  bookedInterval: number;
 }
 
-interface CustomerAttendance {
-  [customerId: string]: Omit<AttendanceRecord, "customerId">[];
+interface CustomerAttendanceMap {
+  [customerId: string]: CustomerAttendanceRecord[];
+}
+
+interface CustomerAttendanceRecord {
+  date: string;
+  attendedInterval: number;
+  bookedInterval: number;
 }
 
 export const getMonthAttendanceVariance = (state: LocalStore) => {
   const {
     app: { calendarDay },
     firestore: {
-      data: {
-        attendance = [],
-        // customers = {}
-      },
+      data: { attendance = [], customers = {} },
     },
   } = state;
 
   const currentMonth = getMonthStr(calendarDay, 0);
 
-  const monthAttendanceRecords = Object.values(attendance)
+  const customerAttendance = Object.values(attendance)
     .filter(filterAttendanceByMonth(currentMonth))
     .reduce(flattenAndConvertAttendanceIntervals, [])
     .reduce(collectAttendanceByCustomer, {});
 
-  return monthAttendanceRecords;
+  const attendanceTableData = Object.entries(customerAttendance).map(
+    formatToTableData(customers)
+  );
+
+  return attendanceTableData;
 };
 
 /**
@@ -46,7 +51,9 @@ export const filterAttendanceByMonth =
 
 /**
  * Flattens nested `SlotAttendance.attendances`
- * and converts attendance interval strings | null to numbners
+ * and converts attendance interval strings | null to numbers
+ * e.g:
+ * `customerAttendance.reduce(flattenAndConvertAttendanceIntervals, [])`
  */
 export const flattenAndConvertAttendanceIntervals = (
   acc: AttendanceRecord[],
@@ -65,14 +72,56 @@ export const flattenAndConvertAttendanceIntervals = (
 
 /**
  * Collect attendance records in a map by Customer ID
+ *  e.g:
+ * `customerAttendance.reduce(collectAttendanceByCustomer, {})`
  */
-const collectAttendanceByCustomer = (
-  acc: CustomerAttendance,
+export const collectAttendanceByCustomer = (
+  acc: CustomerAttendanceMap,
   { customerId, ...attendanceRecord }: AttendanceRecord
 ) => {
   acc[customerId]
     ? acc[customerId].push(attendanceRecord)
     : (acc[customerId] = [attendanceRecord]);
+
+  return acc;
+};
+
+/**
+ * Formats CustomerAttendance records as AttendanceVariance TableData entries by:
+ * - mapping customerId to customer name
+ * - reducing daily attendance to a single number
+ * e.g:
+ * `customerAttendance.map(formatToTableData(customers))`
+ */
+export const formatToTableData =
+  (customers: Record<string, Customer>) =>
+  ([customerId, customerRecords]: [
+    customerId: string,
+    customerRecords: CustomerAttendanceRecord[]
+  ]) => {
+    const athlete = `${customers[customerId]?.name} ${customers[customerId]?.surname}`;
+    const hours = customerRecords.reduce(collectCustomerDailyAttendance, {});
+
+    return { athlete, hours };
+  };
+
+/**
+ * Reduce multiple customer attendance records for a day to a single number,
+ * and collect by date in an object, e.g:
+ * `customerAttendance.reduce(collectAttendanceByCustomer, {})`
+ */
+const collectCustomerDailyAttendance = (
+  acc: DayAttendanceHours,
+  { date, bookedInterval, attendedInterval }: CustomerAttendanceRecord
+) => {
+  const dateEntry = acc[date];
+
+  if (dateEntry) {
+    const [booked, attended] = dateEntry;
+    acc[date] = [booked + bookedInterval, attended + attendedInterval];
+  } else {
+    acc[date] = [bookedInterval, attendedInterval];
+  }
 
   return acc;
 };
