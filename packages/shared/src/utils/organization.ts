c4d4a6bde53dd7b4bf3b7bbcfa2ac989e727031b
Fadwa <fadwa.mahmoud@gmail.com>
2022-12-21 12:27:17 +0200

    Remove unnecessary function

--- packages/shared/src/utils/organization.ts
+++ packages/shared/src/utils/organization.ts
@@ -1,4 +1,4 @@
-import { ClientEmailPayload, EmailType, HTTPSErrors } from "../index";
+import { HTTPSErrors } from "../index";
 export const interpolateEmailTemplate = (
   template: string,
   fieldValues: Record<string, string>
@@ -19,25 +19,4 @@ export const interpolateEmailTemplate = (
     template = template.replace(placeholder, placeholderValue);
   });
   return template;
-};
-
-export const interpolateSendICSEmail = (
-  icsTemplate: { subject: string; html: string },
-  icsRequiredFields: ClientEmailPayload[EmailType.SendCalendarFile]
-) => {
-  const { name, surname } = icsRequiredFields.customer;
-  const subjectRequiredFields = {
-    displayName: icsRequiredFields.displayName,
-  };
-  const htmlRequiredFields = {
-    name: `${name} ${surname}`,
-    icsFile: icsRequiredFields.attachments.filename,
-  };
-  return {
-    subject: interpolateEmailTemplate(
-      icsTemplate.subject,
-      subjectRequiredFields
-    ),
-    html: interpolateEmailTemplate(icsTemplate.html, htmlRequiredFields),
-  };
 };
