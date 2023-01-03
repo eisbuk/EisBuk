c5e19e35b252e88213c896a70da02a0f8d325774
ikusteu <ivan.kust57@gmail.com>
2023-01-03 11:28:00 +0100

    Remove the deprecated form/dialog/card/DateInput components (as they're replaced by new ones)

--- packages/client/src/components/atoms/DateInput/index.tsx
+++
@@ -1,5 +0,0 @@
-import FormikDateInput, { DateInput } from "./DateInput";
-
-export { DateInput };
-
-export default FormikDateInput;
--- packages/client/src/components/customers/CustomerForm/index.ts
+++
@@ -1,5 +0,0 @@
-import CustomerForm, { CustomerFormProps } from "./CustomerForm";
-
-export type { CustomerFormProps };
-
-export default CustomerForm;
