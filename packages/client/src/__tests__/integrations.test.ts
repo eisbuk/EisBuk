f3efa12e7d43eef9c9cb4112128aec9799fa2665
Fadwa <fadwa.mahmoud@gmail.com>
2023-01-11 12:01:00 +0200

    Write tests for opinionated email types

--- packages/client/src/__tests__/integrations.test.ts
+++ packages/client/src/__tests__/integrations.test.ts
@@ -6,6 +6,8 @@ import {
   CustomerFull,
   EmailPayload,
   EmailType,
+  defaultEmailTemplates,
+  interpolateText,
 } from "@eisbuk/shared";
 import {
   DeliveryStatus,
@@ -82,6 +84,224 @@ describe("Email sending and delivery", () => {
       expect(recipients.length).toEqual(2);
       expect(recipients).toContain("saul@gmail.com");
       expect(recipients).toContain("bcc@gmail.com");
+    }
+  );
+  testWithEmulator(
+    "shold deliver an email of type SendBookingsLink with correct email fields",
+    async () => {
+      const { organization } = await setUpOrganization({
+        doLogin: true,
+        setSecrets: true,
+        additionalSetup: {
+          emailFrom: "from@gmail.com",
+        },
+      });
+
+      const recipients: string[] = [];
+
+      // Override 'onRcptTo' to keep track of recipients for an email
+      smtpServer.server.onRcptTo = (
+        addr: { address: string },
+        _: any,
+        done: () => void
+      ) => {
+        recipients.push(addr.address);
+        done();
+      };
+
+      const bookingsLink = "https://eisbuk.it/saul";
+      const payload: ClientEmailPayload[EmailType.SendBookingsLink] = {
+        type: EmailType.SendBookingsLink,
+        organization,
+        customer: saul as Required<CustomerFull>,
+        bookingsLink,
+      };
+
+      const res = await httpsCallable(
+        functions,
+        CloudFunction.SendEmail
+      )(payload);
+
+      const deliveryDocPath = (res.data as any).deliveryDocumentPath;
+
+      // Wait for the process delivery to complete before making further assertions
+      await waitForCondition<ProcessDocument<EmailPayload>>({
+        documentPath: deliveryDocPath,
+        condition: (data) => data?.delivery?.status === DeliveryStatus.Success,
+        verbose: true,
+      });
+
+      // There should be a single email payload, but with multiple recipients ('to' and 'bcc')
+      expect(smtpServer).toHaveReceivedEmails(1);
+      const email = smtpServer.emails[0];
+
+      const interpolatedHtml = interpolateText(
+        defaultEmailTemplates[EmailType.SendBookingsLink].html,
+        {
+          organizationName: organization,
+          name: saul.name,
+          surname: saul.surname,
+          bookingsLink,
+        }
+      );
+      const interpolatedSubject = interpolateText(
+        defaultEmailTemplates[EmailType.SendBookingsLink].subject,
+        {
+          organizationName: organization,
+        }
+      );
+
+      expect(email.html.trim()).toEqual(interpolatedHtml);
+      expect(email.subject.trim()).toEqual(interpolatedSubject);
+      expect(recipients.length).toEqual(2);
+    }
+  );
+  testWithEmulator(
+    "shold deliver an email of type SendCalendarFile with correct email fields",
+    async () => {
+      const { organization } = await setUpOrganization({
+        doLogin: true,
+        setSecrets: true,
+        additionalSetup: {
+          emailFrom: "from@gmail.com",
+        },
+      });
+
+      const recipients: string[] = [];
+
+      // Override 'onRcptTo' to keep track of recipients for an email
+      smtpServer.server.onRcptTo = (
+        addr: { address: string },
+        _: any,
+        done: () => void
+      ) => {
+        recipients.push(addr.address);
+        done();
+      };
+
+      const payload: ClientEmailPayload[EmailType.SendCalendarFile] = {
+        type: EmailType.SendCalendarFile,
+        organization,
+        customer: saul as Required<CustomerFull>,
+        attachments: {
+          filename: "icsFile.ics",
+          content: "icsFile",
+        },
+      };
+
+      const res = await httpsCallable(
+        functions,
+        CloudFunction.SendEmail
+      )(payload);
+
+      const deliveryDocPath = (res.data as any).deliveryDocumentPath;
+
+      // Wait for the process delivery to complete before making further assertions
+      await waitForCondition<ProcessDocument<EmailPayload>>({
+        documentPath: deliveryDocPath,
+        condition: (data) => data?.delivery?.status === DeliveryStatus.Success,
+        verbose: true,
+      });
+
+      // There should be a single email payload, but with multiple recipients ('to' and 'bcc')
+      expect(smtpServer).toHaveReceivedEmails(1);
+      const email = smtpServer.emails[0];
+
+      const interpolatedHtml = interpolateText(
+        defaultEmailTemplates[EmailType.SendCalendarFile].html,
+        {
+          organizationName: organization,
+          name: saul.name,
+          surname: saul.surname,
+          icsFile: "icsFile.ics",
+        }
+      );
+
+      const interpolatedSubject = interpolateText(
+        defaultEmailTemplates[EmailType.SendCalendarFile].subject,
+        {
+          organizationName: organization,
+        }
+      );
+
+      expect(email.subject.trim()).toEqual(interpolatedSubject);
+      expect(email.html.trim()).toEqual(interpolatedHtml);
+      expect(email.attachments[0].contentType).toEqual("text/calendar");
+      expect(email.attachments[0].filename).toEqual("icsFile.ics");
+    }
+  );
+  testWithEmulator(
+    "shold deliver an email of type SendExtendedBookingsDate with correct email fields",
+    async () => {
+      const { organization } = await setUpOrganization({
+        doLogin: true,
+        setSecrets: true,
+        additionalSetup: {
+          emailFrom: "from@gmail.com",
+        },
+      });
+
+      const recipients: string[] = [];
+
+      // Override 'onRcptTo' to keep track of recipients for an email
+      smtpServer.server.onRcptTo = (
+        addr: { address: string },
+        _: any,
+        done: () => void
+      ) => {
+        recipients.push(addr.address);
+        done();
+      };
+
+      const extendedBookingsDate = "04/02";
+      const bookingsMonth = "April";
+
+      const payload: ClientEmailPayload[EmailType.SendExtendedBookingsDate] = {
+        type: EmailType.SendExtendedBookingsDate,
+        organization,
+        customer: saul as Required<CustomerFull>,
+        extendedBookingsDate,
+        bookingsMonth,
+      };
+
+      const res = await httpsCallable(
+        functions,
+        CloudFunction.SendEmail
+      )(payload);
+
+      const deliveryDocPath = (res.data as any).deliveryDocumentPath;
+
+      // Wait for the process delivery to complete before making further assertions
+      await waitForCondition<ProcessDocument<EmailPayload>>({
+        documentPath: deliveryDocPath,
+        condition: (data) => data?.delivery?.status === DeliveryStatus.Success,
+        verbose: true,
+      });
+
+      // There should be a single email payload, but with multiple recipients ('to' and 'bcc')
+      expect(smtpServer).toHaveReceivedEmails(1);
+      const email = smtpServer.emails[0];
+
+      const interpolatedHtml = interpolateText(
+        defaultEmailTemplates[EmailType.SendExtendedBookingsDate].html,
+        {
+          organizationName: organization,
+          name: saul.name,
+          surname: saul.surname,
+          extendedBookingsDate,
+          bookingsMonth,
+        }
+      );
+
+      const interpolatedSubject = interpolateText(
+        defaultEmailTemplates[EmailType.SendExtendedBookingsDate].subject,
+        {
+          name: saul.name,
+        }
+      );
+
+      expect(email.subject.trim()).toEqual(interpolatedSubject);
+      expect(email.html.trim()).toEqual(interpolatedHtml);
     }
   );
 });
