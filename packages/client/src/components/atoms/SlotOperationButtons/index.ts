b92206c05a04c0067420c3a18c4a97bb450128ba
ikusteu <ivankust57@gmail.com>
2023-04-27 21:16:35 +0200

    Update SlotOperationButtons exports

--- packages/client/src/components/atoms/SlotOperationButtons/index.ts
+++ packages/client/src/components/atoms/SlotOperationButtons/index.ts
@@ -1,15 +1,9 @@
 import SlotOperationButtons from "./SlotOperationButtons";
 
-import NewSlot from "./NewSlotButton";
-import EditSlot from "./EditSlotButton";
-import Copy from "./CopyButton";
-import Paste from "./PasteButton";
-import Delete from "./DeleteButton";
-
-export const NewSlotButton = NewSlot;
-export const EditSlotButton = EditSlot;
-export const CopyButton = Copy;
-export const PasteButton = Paste;
-export const DeleteButton = Delete;
+export { default as NewSlotButton } from "./NewSlotButton";
+export { default as EditSlotButton } from "./EditSlotButton";
+export { default as CopyButton } from "./CopyButton";
+export { default as PasteButton } from "./PasteButton";
+export { default as DeleteButton } from "./DeleteButton";
 
 export default SlotOperationButtons;
