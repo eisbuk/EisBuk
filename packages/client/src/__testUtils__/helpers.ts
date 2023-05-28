a5532f6c7f4f566b3f1dc2439030b607814184d6
ikusteu <ivankust57@gmail.com>
2023-05-28 14:31:23 +0200

    Extend default test timeout to reduce flakiness due to emulators sometimes being choked up: * 1000ms (with 8000ms 'waitFor' timeout) for local testing * 15000ms (with 13000ms 'waitFor' timeout) for local testing

--- packages/client/src/__testUtils__/helpers.ts
+++ packages/client/src/__testUtils__/helpers.ts
@@ -1,10 +1,18 @@
+const __isCI__ = process.env.CI;
+
 /**
  * A test helper, runs the callback with 50 ms interval until the assertion is fulfilled or it times out.
  * If it times out, it rejects with the latest error.
  * @param {Function} cb The callback to run (this would normally hold assertions)
- * @param {number} [timeout] The timeout in ms
+ * @param {number} [timeout] The timeout in ms - default is 8000 as our test timeout is 10000 and we assume that most of the
+ * assertions will be fulfilled within the remaining 2 seconds, leaving us room for the problematic ones to wait a bit longer,
+ * whilst still failing within the test timeout constraints (in order to provide the helpful error message, rather than a test timeout error).
  */
-export const waitFor = <CB extends () => any>(cb: CB, timeout = 2000) => {
+export const waitFor = <CB extends () => any>(
+  cb: CB,
+  // The CI machine tends to be slower than the local ones, so we're allowing for more breathing room
+  timeout = __isCI__ ? 13000 : 8000
+) => {
   return new Promise<ReturnType<CB>>((resolve, reject) => {
     let error: any = null;
 
--- packages/client/vitest.config.ts
+++ packages/client/vitest.config.ts
@@ -2,9 +2,22 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const __isCI__ = Boolean(process.env.CI);
+
 export default defineConfig({
   plugins: [react()],
   test: {
+    // We're testing with firebase emulators, which, can be a bit slow
+    // (either when emulators are just being started, as well as when multiple test threads make requests against them),
+    // while our tests are fast.
+    // We could reduce the number of testing threads, to reduce the load on the emulators,
+    // but that would slow down the whole suite significantly (and still leave room for flakiness).
+    // However, we can keep the tests fast, and simply allow for longer timeouts, where the quick and easy tests will finish quickly,
+    // and the tests requiring the emulators to respond will wait a bit longer,
+    // giving us (kinda) zero overhead assurance that the tests won't be flaky:
+    // We pay the performance toll when that's needed to remove flakieness (waiting for emulators to respont),
+    // but there's no toll for tests that don't need it.
+    testTimeout: __isCI__ ? 15000 : 10000,
     setupFiles: ["./vitest.setup.ts"],
   },
   resolve: {
