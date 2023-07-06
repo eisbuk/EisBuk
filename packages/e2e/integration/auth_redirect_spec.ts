6a77c8f03cffb98870d53226227a6ca5c57ec381
ikusteu <ivankust57@gmail.com>
2023-07-06 22:03:07 +0200

    Write e2e test suite testing auth-related redirects

---
+++ packages/e2e/integration/auth_redirect_spec.ts
@@ -0,0 +1,132 @@
+import { type Customer } from "@eisbuk/shared";
+import i18n, { AttendanceNavigationLabel } from "@eisbuk/translations";
+
+import { PrivateRoutes, Routes } from "../temp";
+
+import { customers } from "../__testData__/customers.json";
+
+describe("auth-related redirects", () => {
+  beforeEach(() => {
+    // Initialize app, create default user,
+    // create default organization but don't sign in
+    cy.initAdminApp().then((organization) =>
+      cy.updateCustomers(organization, customers as Record<string, Customer>)
+    );
+  });
+
+  afterEach(() => {
+    cy.signOut().then(() => cy.clearLocalStorage());
+  });
+
+  describe("non authenticated user", () => {
+    it("redirects a non authenticated user to a login page", () => {
+      // Check for /athletes page
+      cy.visit(PrivateRoutes.Athletes);
+      cy.url().should("include", "/login");
+
+      // Check for /athletes/new page
+      cy.visit(PrivateRoutes.NewAthlete);
+      cy.url().should("include", "/login");
+
+      // Check for /athletes/:id page
+      cy.visit([PrivateRoutes.Athletes, customers.saul.id].join("/"));
+      cy.url().should("include", "/login");
+
+      // Check for attendance ("/") page
+      cy.visit(PrivateRoutes.Root);
+      cy.url().should("include", "/login");
+
+      // Check for slots page
+      cy.visit(PrivateRoutes.Slots);
+      cy.url().should("include", "/login");
+
+      // Check for admin preferences page
+      cy.visit(PrivateRoutes.AdminPreferences);
+      cy.url().should("include", "/login");
+    });
+
+    it("allows non authenticated user, in possession of secret key, to access bookings page for a customer in possession of said secret key", () => {
+      cy.visit([Routes.CustomerArea, customers.saul.secretKey].join("/"));
+      // We check the customer area page being loaded by looking for the customer avatar (with name)
+      cy.contains(`${customers.saul.name} ${customers.saul.surname}`);
+    });
+  });
+
+  describe("authenticated non-admin", () => {
+    it("redirects to customer area page from any of the private routes", () => {
+      const { email, name, surname } = customers.saul;
+
+      // Sign up (or sign in on each subsequent test) as saul.
+      // Saul is already registered in the app (in customers collection).
+      cy.signUp(email, "123456");
+
+      // Check for /athletes page
+      cy.visit(PrivateRoutes.Athletes);
+      cy.contains(`${name} ${surname}`);
+
+      // Check for /athletes/new page
+      cy.visit(PrivateRoutes.NewAthlete);
+      cy.contains(`${name} ${surname}`);
+
+      // Check for /athletes/:id page
+      cy.visit([PrivateRoutes.Athletes, customers.saul.id].join("/"));
+      cy.contains(`${name} ${surname}`);
+
+      // Check for attendance ("/") page
+      cy.visit(PrivateRoutes.Root);
+      cy.contains(`${name} ${surname}`);
+
+      // Check for slots page
+      cy.visit(PrivateRoutes.Slots);
+      cy.contains(`${name} ${surname}`);
+
+      // Check for admin preferences page
+      cy.visit(PrivateRoutes.AdminPreferences);
+      cy.contains(`${name} ${surname}`);
+    });
+  });
+
+  describe("authenticated, but not fully registered (no secret key)", () => {
+    it("should redirect to /self_register page to complete the athlete registration", () => {
+      // Sign up (or sign in on each subsequent test) as a random user, which
+      // doesn't exist in the customers collection (has no secret key).
+      cy.signUp("some@email.com", "passw03d");
+
+      // Check for /athletes page
+      cy.visit(PrivateRoutes.Athletes);
+      cy.url().should("include", "/self_register");
+
+      // Check for /athletes/new page
+      cy.visit(PrivateRoutes.NewAthlete);
+      cy.url().should("include", "/self_register");
+
+      // Check for /athletes/:id page
+      cy.visit([PrivateRoutes.Athletes, customers.saul.id].join("/"));
+      cy.url().should("include", "/self_register");
+
+      // Check for attendance ("/") page
+      cy.visit(PrivateRoutes.Root);
+      cy.url().should("include", "/self_register");
+
+      // Check for slots page
+      cy.visit(PrivateRoutes.Slots);
+      cy.url().should("include", "/self_register");
+
+      // Check for admin preferences page
+      cy.visit(PrivateRoutes.AdminPreferences);
+      cy.url().should("include", "/self_register");
+    });
+  });
+
+  describe("authenticated admin", () => {
+    it("redirects to attendance page if authenticated admin opens the login page", () => {
+      // Sign in with default user (admin)
+      cy.signIn();
+      cy.visit(Routes.Login);
+
+      // Check that we're redirected to attendance page by checking for attendance page sub-nav
+      cy.contains(i18n.t(AttendanceNavigationLabel.Day) as string);
+      cy.contains(i18n.t(AttendanceNavigationLabel.Month) as string);
+    });
+  });
+});
