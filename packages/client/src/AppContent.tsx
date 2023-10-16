94a24c7b8fcfd5663639796e84b517e882960b9c
ikusteu <ivankust57@gmail.com>
2023-10-16 09:15:26 +0200

    Update firebase-emulators-warning:
    * Remove the firebase-emulator-warning element attached to the DOM by the presence of emulators
    * Create our own version of the warning - one that could be removed to not impede on the styles of the app

--- packages/client/src/AppContent.tsx
+++ packages/client/src/AppContent.tsx
@@ -29,6 +29,7 @@ import AdminPreferencesPage from "@/pages/admin_preferences";
 import SelfRegister from "@/pages/self_register";
 
 import { getIsAdmin } from "@/store/selectors/auth";
+import { DevWarning } from "@eisbuk/ui";
 
 /**
  * All of the App content (including routes) wrapper.
@@ -52,58 +53,76 @@ const AppContent: React.FC = () => {
   useFirestoreSubscribe(getOrganization(), subscribedCollections);
   usePaginateFirestore();
 
-  return (
-    <Switch>
-      <LoginRoute path={Routes.Login} component={LoginPage} />
-      <PrivateRoute
-        exact
-        path={PrivateRoutes.Root}
-        component={AttendancePage}
-      />
-      <PrivateRoute
-        exact
-        path={PrivateRoutes.Athletes}
-        component={AthletesPage}
-      />
-      <PrivateRoute
-        exact
-        path={PrivateRoutes.NewAthlete}
-        component={AthleteProfilePage}
-      />
-      <PrivateRoute
-        path={`${PrivateRoutes.Athletes}/:athlete`}
-        component={AthleteProfilePage}
-      />
-      <PrivateRoute path={PrivateRoutes.Slots} component={SlotsPage} />
-      <PrivateRoute
-        path={Routes.AttendancePrintable}
-        component={AttendancePrintable}
-      />
-      <PrivateRoute
-        path={PrivateRoutes.AdminPreferences}
-        component={AdminPreferencesPage}
-      />
+  // Remove the firestore emulator warning as it's in the way.
+  // We're adding a warning of our own, incorporated nicely into out layout
+  React.useEffect(() => {
+    document.querySelector("p.firebase-emulator-warning")?.remove();
+  }, []);
 
-      <PrivateRoute
-        // Private route is a hack here...if visiting '/customer_area' without a secret key,
-        // if will handle all cases of auth/non-auth/auth-but-not-registered appropriately.
-        //
-        // For admin, however, after they pass the PrivateRoute checks, they will be redirected to
-        // '/athletes' page (from where they can redirect to the correct customer area - for a given customer)
-        path={Routes.CustomerArea}
-        exact={true}
-      >
-        <Redirect to={PrivateRoutes.Athletes} />
-      </PrivateRoute>
-      <Route
-        path={`${Routes.CustomerArea}/:secretKey`}
-        component={CustomerAreaPage}
-      />
-      <Route exact path={Routes.ErrorBoundary} component={ErrorBoundaryPage} />
-      <Route path={Routes.SelfRegister} component={SelfRegister} exact />
-      <Route path={Routes.Debug} component={DebugPage} />
-      <Route path={Routes.Deleted} component={Deleted} />
-    </Switch>
+  const isDev = process.env.NODE_ENV === "development";
+
+  return (
+    <>
+      <Switch>
+        <LoginRoute path={Routes.Login} component={LoginPage} />
+        <PrivateRoute
+          exact
+          path={PrivateRoutes.Root}
+          component={AttendancePage}
+        />
+        <PrivateRoute
+          exact
+          path={PrivateRoutes.Athletes}
+          component={AthletesPage}
+        />
+        <PrivateRoute
+          exact
+          path={PrivateRoutes.NewAthlete}
+          component={AthleteProfilePage}
+        />
+        <PrivateRoute
+          path={`${PrivateRoutes.Athletes}/:athlete`}
+          component={AthleteProfilePage}
+        />
+        <PrivateRoute path={PrivateRoutes.Slots} component={SlotsPage} />
+        <PrivateRoute
+          path={Routes.AttendancePrintable}
+          component={AttendancePrintable}
+        />
+        <PrivateRoute
+          path={PrivateRoutes.AdminPreferences}
+          component={AdminPreferencesPage}
+        />
+
+        <PrivateRoute
+          // Private route is a hack here...if visiting '/customer_area' without a secret key,
+          // if will handle all cases of auth/non-auth/auth-but-not-registered appropriately.
+          //
+          // For admin, however, after they pass the PrivateRoute checks, they will be redirected to
+          // '/athletes' page (from where they can redirect to the correct customer area - for a given customer)
+          path={Routes.CustomerArea}
+          exact={true}
+        >
+          <Redirect to={PrivateRoutes.Athletes} />
+        </PrivateRoute>
+        <Route
+          path={`${Routes.CustomerArea}/:secretKey`}
+          component={CustomerAreaPage}
+        />
+        <Route
+          exact
+          path={Routes.ErrorBoundary}
+          component={ErrorBoundaryPage}
+        />
+        <Route path={Routes.SelfRegister} component={SelfRegister} exact />
+        <Route path={Routes.Debug} component={DebugPage} />
+        <Route path={Routes.Deleted} component={Deleted} />
+      </Switch>
+
+      <div className="fixed z-50 bottom-1 left-1/2 -translate-x-1/2 content-container text-center">
+        <DevWarning open={isDev} />
+      </div>
+    </>
   );
 };
 
