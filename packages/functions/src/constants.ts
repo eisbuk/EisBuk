5b7e433e359b6a3e9d3fb749999be7209f5777d0
Silvio Tomatis <silviot@gmail.com>
2024-01-16 14:40:09 +0100

    Put the sentry dsn in functions environment file before deploying

--- packages/functions/src/constants.ts
+++ packages/functions/src/constants.ts
@@ -1,4 +1,13 @@
+import * as Sentry from "@sentry/serverless";
+
 export const __functionsZone__ = "europe-west6";
 export const __projectId__ = process.env.PROJECT_ID;
 export const __smsUrl__ = "https://gatewayapi.com/rest/mtsms";
-export const __sentryDSN__ = process.env.REACT_APP_SENTRY_DSN;
+export const __sentryDSN__ = process.env.FUNCTIONS_SENTRY_DSN;
+
+if (!process.env.FUNCTIONS_EMULATOR) {
+  Sentry.init({
+    dsn: __sentryDSN__,
+    tracesSampleRate: 1.0,
+  });
+}
