ce122d383c9f89dc46908acd736074f94c1a20d9
Silvio Tomatis <silviot@gmail.com>
2024-01-16 22:36:25 +0100

    Use different sentry DSN

--- packages/functions/src/constants.ts
+++ packages/functions/src/constants.ts
@@ -3,7 +3,7 @@ import * as Sentry from "@sentry/serverless";
 export const __functionsZone__ = "europe-west6";
 export const __projectId__ = process.env.PROJECT_ID;
 export const __smsUrl__ = "https://gatewayapi.com/rest/mtsms";
-export const __sentryDSN__ = process.env.REACT_APP_SENTRY_DSN;
+export const __sentryDSN__ = process.env.FUNCTIONS_SENTRY_DSN;
 
 if (!process.env.FUNCTIONS_EMULATOR) {
   Sentry.init({
