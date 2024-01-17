d22d97bff8d722965aad3e899000253d80c6f027
Silvio Tomatis <silviot@gmail.com>
2024-01-17 11:53:56 +0100

    Make sure sync functions also get their exceptions reported to sentry

--- packages/functions/src/sentry-serverless-firebase.ts
+++ packages/functions/src/sentry-serverless-firebase.ts
@@ -136,19 +136,25 @@ function wrap<A, B, C>(
     });
     scope.setSpan(transaction);
 
-    // @ts-expect-error - I'm sorry, I lifted this code and I don't know how to fix this typing error
-    return (
-      Promise.resolve(fn(a, b))
-        .catch((err): void => {
-          captureException(err, { tags: { handled: false } });
-          throw err;
-        })
-        // eslint-disable-next-line promise/no-return-in-finally
-        .finally((): Promise<boolean> => {
-          transaction.finish();
-          return flush(2000);
-        }) as Promise<C | undefined>
-    );
+    try {
+      const result = fn(a, b);
+      // @ts-expect-error - I'm sorry, I lifted this code and I don't know how to fix this typing error
+      return (
+        Promise.resolve(result)
+          .catch((err): void => {
+            captureException(err, { tags: { handled: false } });
+            throw err;
+          })
+          // eslint-disable-next-line promise/no-return-in-finally
+          .finally((): Promise<boolean> => {
+            transaction.finish();
+            return flush(2000);
+          }) as Promise<C | undefined>
+      );
+    } catch (err) {
+      captureException(err, { tags: { handled: false } });
+      throw err;
+    }
   };
 }
 
