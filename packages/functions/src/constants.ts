1cd847d049f3bcaed23dce8e74a2d9eb2f5c04bb
Silvio Tomatis <silviot@gmail.com>
2024-01-31 11:48:31 +0100

    Use a sentry release in cloud functions too

--- packages/functions/src/constants.ts
+++ packages/functions/src/constants.ts
@@ -6,12 +6,14 @@ export const __smsUrl__ = "https://gatewayapi.com/rest/mtsms";
 export const __isEmulator__ = process.env.FUNCTIONS_EMULATOR === "true";
 
 export const __sentryDSN__ = process.env.FUNCTIONS_SENTRY_DSN;
+export const __sentryRelease__ = process.env.REACT_APP_SENTRY_RELEASE;
 export const __enableSentry__ =
   !process.env.FUNCTIONS_EMULATOR && Boolean(__sentryDSN__);
 
 if (__enableSentry__) {
   Sentry.init({
     dsn: __sentryDSN__,
+    release: __sentryRelease__,
     tracesSampleRate: 1.0,
   });
 }
